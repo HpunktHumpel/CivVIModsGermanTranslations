5595fd312ab984b64feff810527e03aa244dbd0c
gorowynn <36330734+gorowynn@users.noreply.github.com>
2021-11-02 20:50:52 +0100

    Added UC Entertainment
    Translation of Urban Complexity - Entertainment Module

---
+++ CivVIModsGermanTranslations/2474028548/Text/ModSupport/UC_ENT_Text_CS_CSE_Default_de_DE.sql
@@ -0,0 +1,18 @@
+-- UC_CMP_Text_CS_CSE_Default
+-- Author: JNR
+--Translation: Gorowynn
+--------------------------------------------------------------
+
+-- German
+--------------------------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,															Language,	Text)
+VALUES	('LOC_CSE_ENTERTAINMENT_TRAIT_MEDIUM_INFLUENCE_BONUS',			'de_DE',	'+1 [ICON_AMENITIES] Annehmlichkeit für jede Arena, Turnierplatz, Yachthafen und Riesenrad.'),
+		('LOC_CSE_ENTERTAINMENT_TRAIT_LARGE_INFLUENCE_BONUS',			'de_DE',	'+1 [ICON_AMENITIES] Annehmlichkeit für jeden Zoo, Botanischen Garten, Aquarium und Casino.'),
+		('LOC_CSE_ENTERTAINMENT_TRAIT_LARGEST_INFLUENCE_BONUS',			'de_DE',	'+1 [ICON_AMENITIES] Annehmlichkeit für jedes Stadion, Kongresszentrum, Konzerthalle und Schwimmbad.');
+
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,															Language,	Text)
+VALUES	('LOC_CSE_ENTERTAINMENT_TRAIT_MEDIUM_INFLUENCE_MEKONG_BONUS',	'de_DE',	'+1 [ICON_AMENITIES] Annehmlichkeit für jede Arena, Turnierplatz, Yachthafen und Riesenrad.'),
+		('LOC_CSE_ENTERTAINMENT_TRAIT_LARGEST_INFLUENCE_MEKONG_BONUS',	'de_DE',	'+1 [ICON_AMENITIES] Annehmlichkeit für jedes Stadion, Kongresszentrum, Konzerthalle und Schwimmbad.');
+--------------------------------------------------------------
