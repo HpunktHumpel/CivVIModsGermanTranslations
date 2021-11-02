034279c7f464e392c5ca2b5388cc52d49a447183
gorowynn <36330734+gorowynn@users.noreply.github.com>
2021-11-02 20:37:38 +0100

    Added UC - Aqueducts
    Translation for Urban Complexity - Aqueducts

---
+++ CivVIModsGermanTranslations/2362219368/Text/ModSupport/UC_AQD_Text_CS_CSE_Default_de_DE.sql
@@ -0,0 +1,16 @@
+-- UC_AQD_Text_CS_CSE_Default
+-- Author: JNR
+-- Translation: Gorowynn
+--------------------------------------------------------------
+
+-- German
+--------------------------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,													Language,	Text)
+VALUES	('LOC_CSE_AGRICULTURAL_TRAIT_LARGE_INFLUENCE_BONUS',	'de_DE',	'+2 [ICON_Food] Nahrung in jedem Obstgarten, Hammerwerk oder Badehaus.'),
+		('LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_BONUS',	'de_DE',	'+2 [ICON_Food] Nahrung in jeder Kanalisation.');
+
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,															Language,	Text)
+VALUES	('LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_MEKONG_BONUS',	'de_DE',	'+3 [ICON_Food] Nahrung in jeder Kanalisation.');
+--------------------------------------------------------------
---
+++ CivVIModsGermanTranslations/2362219368/Text/ModSupport/UC_AQD_Text_CS_CSE_Ethiopia_de_DE.sql
@@ -0,0 +1,18 @@
+-- UC_AQD_Text_CS_CSE_Ethiopia
+-- Author: JNR
+-- Translation: Gorowynn
+--------------------------------------------------------------
+
+-- German
+--------------------------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,													Language,	Text)
+VALUES	('LOC_CSE_AGRICULTURAL_TRAIT_MEDIUM_INFLUENCE_BONUS',	'de_DE',	'+2 [ICON_Food] Nahrung durch jeden Kornspeicher in Städten mit einer Zisterne.'),
+		('LOC_CSE_AGRICULTURAL_TRAIT_LARGE_INFLUENCE_BONUS',	'de_DE',	'+3 [ICON_Food] Nahrung durch jeden Obstgarten, Hammerwerk oder Badehaus.'),
+		('LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_BONUS',	'de_DE',	'+4 [ICON_Food] Nahrung durch jede Kanalisation.');
+
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,															Language,	Text)
+VALUES	('LOC_CSE_AGRICULTURAL_TRAIT_MEDIUM_INFLUENCE_MEKONG_BONUS',	'de_DE',	'+2 [ICON_Food] Nahrung durch jeden Kornspeicher in Städten mit einer Zisterne.'),
+		('LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_MEKONG_BONUS',	'de_DE',	'+4 [ICON_Food] Nahrung durch jede Kanalisation.');
+--------------------------------------------------------------
