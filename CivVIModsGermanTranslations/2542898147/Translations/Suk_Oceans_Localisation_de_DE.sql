1c2fdef5a3d28ac58157fe53f1b5126c02f823b3
H.Humpel <H.Humpel@gmx.de>
2021-11-28 23:50:57 +0100

    New mod added: Sukritact's Oceans

---
+++ CivVIModsGermanTranslations/2542898147/Translations/Suk_Oceans_Localisation_de_DE.sql
@@ -0,0 +1,155 @@
+--========================================================================================================================
+-- LocalizedText
+--========================================================================================================================
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,								Language,		Text)
+VALUES
+--========================================================================================================================
+-- Game Mode
+--========================================================================================================================
+		(
+			"LOC_GAMEMODE_SUK_OCEANS_NAME",
+			"de_DE",
+			"Sukritact’s Oceans"
+		),
+		(
+			"LOC_GAMEMODE_SUK_OCEANS_DESCRIPTION",
+			"de_DE",
+			"Dieser Spielmodus haucht den Weltmeeren neues Leben ein, mit neuen Ressourcen, neuen Elementen und neuem Spielverhalten.[NEWLINE][NEWLINE]An den Küsten wimmelt es von Kelp-Wäldern, die das Wachstum fördern und die Flotten verlangsamen. Neue Ressourcen wie Robben, Tintenfische und Korallen warten jetzt in den Tiefen und sorgen für mehr Abwechslung. Küstenluxus ist jetzt für jeden Kontinent anders, so dass neue Ozeane neue Reichtümer hervorbringen werden. Modernisierungen und Bezirke, wie z.B. die Fischerei und der Wasserpark, wurden an diese Änderungen angepasst."
+		),
+--========================================================================================================================
+-- Squid
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_SQUID_NAME",
+			"de_DE",
+			"Tintenfisch"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SQUID_CHAPTER_HISTORY_PARA_1",
+			"de_DE",
+			"Tintenfische gehören zu einer besonders erfolgreichen Gruppe von Weichtieren, den Kopffüßern, die es seit etwa 500 Millionen Jahren gibt. Tintenfische sind in den Ozeanen der ganzen Welt zu finden. Einige Tintenfische bevorzugen wärmere, tropische Gewässer, während andere in den kalten Meeren gedeihen. Tintenfische sind schnelle Schwimmer, die sich mit Hilfe von Strahlenantrieb fortbewegen und ihre Beute größtenteils durch Sichtkontakt aufspüren. Sie gehören zu den intelligentesten wirbellosen Tieren, und man hat beobachtet, dass Gruppen von Humboldt-Tintenfischen gemeinsam jagen. Sie werden von Haien, anderen Fischen, Seevögeln, Robben und Walen gefressen."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SQUID_CHAPTER_HISTORY_PARA_2",
+			"de_DE",
+			"Tintenfische sind eine wichtige Nahrungsquelle und werden in den Küchen der ganzen Welt verwendet. In englischsprachigen Ländern werden Tintenfische oft als Calamari bezeichnet, die im 17. Jahrhundert aus dem Italienischen ins Englische übernommen wurden. Tintenfische sind in bestimmten Gebieten reichlich vorhanden und liefern große Fangmengen für die Fischerei. Der Körper kann im Ganzen gefüllt, in flache Stücke geschnitten oder in Ringe geschnitten werden. Die Arme, Tentakel und die Tinte sind ebenfalls essbar; die einzigen Teile, die nicht gegessen werden, sind der Schnabel und der Gladius. Tintenfisch ist eine gute Nahrungsquelle für Zink und Mangan und enthält viel Kupfer, Selen, Vitamin B12 und Riboflavin."
+		),
+--========================================================================================================================
+-- Seals
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_SEALS_NAME",
+			"de_DE",
+			"Robben"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SEALS_CHAPTER_HISTORY_PARA_1",
+			"de_DE",
+			"Heute gibt es 33 Arten von Flossentieren, von denen die meisten als Robben bekannt sind. Sie sind auf allen Kontinenten der Erde zu finden, wobei die meisten Arten in Kaltwasserumgebungen vorkommen. Obwohl es viele Unterschiede zwischen den Arten gibt, haben alle Robben flossenförmige Füße. Das Wort Pinniped bedeutet im Lateinischen flossenfüßig. Die Größe der Robben ist sehr unterschiedlich und reicht von der riesigen Südlichen Seeelefantenrobbe, die mehr wiegen kann als ein Kleintransporter, bis zur relativ schlanken, 100 Pfund schweren Baikalrobbe."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SEALS_CHAPTER_HISTORY_PARA_2",
+			"de_DE",
+			"In der Vergangenheit haben Jäger gezielt Jagd auf die Felle der Flossenrobben gemacht, eine Praxis, die einige Arten zum Aussterben brachte: Die Karibische Mönchsrobbe zum Beispiel verschwand in den 1970er Jahren von der Erde. Obwohl die Jagd heute besser reguliert ist, sind die Tiere immer noch vielen Bedrohungen ausgesetzt, darunter Nahrungsmangel, Verfangen in Fanggeräten und Konflikte mit Fischern. Einige Arten, wie die hawaiianische Mönchsrobbe und die Mittelmeer-Mönchsrobbe, sind nach wie vor vom Aussterben bedroht. Der Klimawandel stellt jedoch die größte Bedrohung für viele Arten von Flossentieren dar, insbesondere für diejenigen, die auf das Meereis angewiesen sind."
+		),
+--========================================================================================================================
+-- Coral
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_CORAL_NAME",
+			"de_DE",
+			"Koralle"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CORAL_CHAPTER_HISTORY_PARA_1",
+			"de_DE",
+			"Der Ozean und der Strand haben Künstlern schon immer einen endlosen Inspirationsschub beschert. Rot, Rosa, Orange und Schwarz gehören zu den begehrtesten Farben von Korallen, die für Schmuck verwendet werden. Die geernteten Korallen werden in der Regel zu Cabochons geschnitten und poliert, zu Perlen geformt oder geschnitzt. Bei dem Teil der Koralle, der für Schmuck verwendet wird, handelt es sich eigentlich nicht um den lebenden Organismus, sondern um seine Karbonatausscheidungen, die die Struktur bilden, auf der die Polypen leben, und obwohl es sich nicht um den lebenden Teil des Tieres handelt, ist es für sein Überleben unerlässlich."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CORAL_CHAPTER_HISTORY_PARA_2",
+			"de_DE",
+			"Heute ist die Verwendung von Korallen in Schmuckstücken umstritten. Korallenschmuck ist nach wie vor beliebt, und alte Stücke erzielen bei Auktionen astronomische Preise. Während einige Juweliere weiterhin Korallen in ihren Entwürfen verwenden, verwenden viele Juweliere nur Korallen, die von alten Stücken wiederverwendet wurden, und andere haben die Herstellung von Korallenschmuck ganz eingestellt. Wenn weniger Juweliere Korallen verwenden, haben die Riffe vielleicht eine bessere Chance, sich zu erholen. Bis dahin können Korallenliebhaber schöne antike Schmuckstücke aus Korallen kaufen oder, wenn sie Glück haben, Korallen in freier Wildbahn sehen."
+		),
+--========================================================================================================================
+-- Lobster
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_LOBSTER_NAME",
+			"de_DE",
+			"Hummer"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_LOBSTER_CHAPTER_HISTORY_PARA_1",
+			"de_DE",
+			"Hummer sind eine Familie großer mariner Krustentiere. Sie haben lange Körper mit muskulösen Schwänzen und leben in Spalten oder Höhlen auf dem Meeresboden. Drei ihrer fünf Beinpaare haben Scheren, darunter das erste Paar, das in der Regel viel größer als die anderen ist. Hummer werden als Meeresfrüchte sehr geschätzt und sind von großer wirtschaftlicher Bedeutung. In den von ihnen besiedelten Küstengebieten sind sie oft eine der profitabelsten Waren."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_LOBSTER_CHAPTER_HISTORY_PARA_2",
+			"de_DE",
+			"Hummer wird in der Regel in der Schale gekocht oder gedünstet serviert. Die Gäste knacken die Schale mit Hummerknackern und fischen das Fleisch mit Hummerpickern heraus. Das Fleisch wird oft mit zerlassener Butter und Zitronensaft gegessen. Hummer wird auch für Suppe, Biskuit, Hummerrollen, Cappon magro und Gerichte wie Hummer Newberg und Hummer Thermidor verwendet."
+		),
+--========================================================================================================================
+-- Caviar
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_CAVIAR_NAME",
+			"de_DE",
+			"Kaviar"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CAVIAR_CHAPTER_HISTORY_PARA_1",
+			"de_DE",
+			"Wenn es ein Lebensmittel gibt, das mit purem Luxus assoziiert wird, dann ist es Kaviar. Diese Delikatesse aus Störeiern ist selten und teuer und gilt in der kulinarischen Welt als begehrtes Gut. Kaviar stammt von verschiedenen Störarten, aber Beluga-Kaviar ist der größte, seltenste und teuerste Kaviar. Bei einem Preis von fast 3.500 Dollar pro Pfund verdient er seinen Spitznamen -schwarzes Gold-. Je nach Land kann Kaviar auch als Bezeichnung für den Rogen anderer Fische wie Lachs, Steelhead, Forelle, Seehase, Felchen oder Karpfen verwendet werden."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CAVIAR_CHAPTER_HISTORY_PARA_2",
+			"de_DE",
+			"Die Störpopulationen sind weltweit geschrumpft, was zum Teil auf die Beliebtheit von Kaviar als Genussmittel zurückzuführen ist, und vielerorts gibt es inzwischen strenge Fangbeschränkungen oder sogar Verbote. Weitere Gründe für den Bestandsrückgang sind die Wasserverschmutzung und der Verlust von Lebensraum durch Flussdämme, die die Fische daran hindern, ihre Laichgründe zu erreichen. Viele Störarten stehen auf der Liste der gefährdeten Arten. Um den Druck von den schwindenden Störpopulationen in freier Wildbahn zu nehmen, gibt es in 20 Ländern Unternehmen, die Störe züchten. In den allermeisten Fällen werden die Eier nach dem Töten der Fische entfernt. Andere Unternehmen versuchen einen nachhaltigeren Ansatz, indem sie die Fische aufziehen, bis sie laichreif sind, und dann, nachdem sie die Eier per Kaiserschnitt entnommen haben, die Fische wieder zunähen und sie für eine weitere Produktion aufziehen. Dieses Verfahren ist sehr hart für die Fische und wurde in mehreren Ländern verboten."
+		),
+--========================================================================================================================
+-- Kelp Forest
+--========================================================================================================================
+		(
+			"LOC_FEATURE_SUK_KELP_NAME",
+			"de_DE",
+			"Seetang-Wald"
+		),
+		(
+			"LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_KELP_CHAPTER_HISTORY_PARA_1",
+			"de_DE",
+			"Kelp sind große Braunalgen, die in kühlen, relativ flachen Gewässern in Küstennähe leben. Sie wachsen in dichten Gruppen, ähnlich wie ein Wald an Land. Diese Wälder weisen eine größere Vielfalt und Dichte an Pflanzen und Tieren auf als fast alle anderen Meeresgemeinschaften. Diese abgestuften Wälder bilden einen attraktiven Lebensraum für eine Vielzahl von Arten und dienen Tausenden von Fischen, Wirbellosen und Meeressäugern als Nahrungsquelle und Zufluchtsort."
+		),
+--========================================================================================================================
+-- Water Park Changes
+--========================================================================================================================
+		(
+			"LOC_DISTRICT_WATER_ENTERTAINMENT_COMPLEX_DESCRIPTION",
+			"de_DE",
+			"Ein Bezirk, der auf einem Küstenfeld errichtet wird, um das Volk bei Laune zu halten, indem er die [ICON_Amenities] Annehmlichkeiten erhöht. Wenn er neben einem Kelpwald gebaut wird, erhält er +1 [ICON_Amenities] Annehmlichkeiten. Kann nicht in einer Stadt mit einem Unterhaltungskomplex gebaut werden. Kann nicht auf einem Riff gebaut werden."
+		),
+		(
+			"LOC_BUILDING_AQUARIUM_DESCRIPTION",
+			"de_DE",
+			"Bonus [ICON_Amenities] Annehmlichkeiten erstrecken sich auf jedes Stadtzentrum innerhalb von 9 Feldern. Dieser Bonus gilt einmal für eine Stadt, und mehrere Kopien dieses Gebäudes innerhalb von 9 Feldern eines Stadtzentrums bieten keine zusätzlichen Boni. +1 [ICON_Science] Wissenschaft auf jedes Riff-, Kelpwald- oder Schiffswrack-Feld in dieser Stadt. Zusätzlich +1 [ICON_Science] Wissenschaft für diese, wenn sie an den Wasserpark angrenzen."
+		),
+		(
+			"LOC_BUILDING_AQUATICS_CENTER_DESCRIPTION",
+			"de_DE",
+			"Bonus [ICON_Amenities] Annehmlichkeiten erstrecken sich auf jedes Stadtzentrum innerhalb von 9 Feldern. Dieser Bonus gilt einmal für eine Stadt, und mehrere Kopien dieses Gebäudes innerhalb von 9 Feldern eines Stadtzentrums bieten keine zusätzlichen Boni. Erhält +2 [ICON_Tourism] Tourismus für jede angrenzende Geländeart an der Küste."
+		),
+--========================================================================================================================
+-- Fishery Changes
+--========================================================================================================================
+		(
+			"LOC_GOVERNOR_PROMOTION_AQUACULTURE_DESCRIPTION",
+			"de_DE",
+			"Kann die Fischerei-Modernisierung errichten. +1 [ICON_Housing] Wohnen. Fischerboote, die an eine Fischerei grenzen, bringen +2 [ICON_Food] Nahrung; und weitere +1 [ICON_Production] Produktion, solange Liang in dieser Stadt ansässig ist."
+		),
+		(
+			"LOC_IMPROVEMENT_FISHERY_DESCRIPTION",
+			"de_DE",
+			"Eine einzigartige [ICON_Gouverneur]-Gouverneursverbesserung, die in einer Stadt mit einem Landvermesser-Gouverneur mit der Aquakultur-Beförderung gebaut werden kann.[NEWLINE][NEWLINE]Dieses Feld kann nicht bearbeitet werden. Fischerboote, die an eine oder mehrere Fischereien angrenzen, bringen +2 [ICON_Food] Nahrung und weitere +1 [ICON_Production] Produktion, solange Liang in dieser Stadt ansässig ist. +1 [ICON_Housing] Wohnraum. Müssen auf einem Küsten- oder Seegeländefeld platziert werden, das an eine Meeresressource angrenzt."
+		);
+--========================================================================================================================
+--========================================================================================================================
