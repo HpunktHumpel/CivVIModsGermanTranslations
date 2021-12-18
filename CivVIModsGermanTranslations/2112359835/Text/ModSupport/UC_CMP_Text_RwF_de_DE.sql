64f37d81acf4a4b7834976ce58cb12261c47c71d
H.Humpel <H.Humpel@gmx.de>
2021-12-18 23:55:29 +0100

    [UPDATE] District Expansion: Campus

---
+++ CivVIModsGermanTranslations/2112359835/Text/ModSupport/UC_CMP_Text_RwF_de_DE.sql
@@ -0,0 +1,14 @@
+-- UC_CMP_Text_RwF
+-- Author: JNR
+-- Translation: H.Humpel
+--------------------------------------------------------------
+
+-- German
+
+--------------------------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,															Language,	Text)
+VALUES	('LOC_POLICY_JFD_SCHOLASTIC_METHOD_DESCRIPTION_UC_JNR',		'de_DE',	'+100% [ICON_SCIENCE] Nachbarschaftsbonus für Campus-Bezirke (+50% in Campus-Bezirken mit einer Universität).'),
+	('LOC_POLICY_JFD_SCIENCE_ACADEMIES_DESCRIPTION_UC_JNR',		'de_DE',	'+100% [ICON_SCIENCE] Nachbarschaftsbonus für Campus-Bezirke (+50% in Campus-Bezirken mit einer Universität). +100% [ICON_PRODUCTION] Nachbarschaftsbonus für Industriegebiet-Bezirke'),
+	('LOC_POLICY_JFD_RATIONAL_BUREAUCRACY_DESCRIPTION_UC_JNR',	'de_DE',	'+1 [ICON_SCIENCE] Wissenschaft durch jedes Gebäude in einem Campus-Bezirken.');
+--------------------------------------------------------------
