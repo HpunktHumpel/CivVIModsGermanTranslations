caf6e61c01c7b9946ddd3c2e39f5adb96c18dff5
H.Humpel <H.Humpel@gmx.de>
2021-12-20 11:35:47 +0100

    New mod added: Requested Pantheons

---
+++ CivVIModsGermanTranslations/1888611021/Pantheons_Text_de_DE.sql
@@ -0,0 +1,25 @@
+--------------------------------------------------------------
+-- Pantheons_Text
+-- Author: Angryr
+-- DateCreated: 10/13/2019 11:52:41 AM
+--------------------------------------------------------------
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('de_DE', 'LOC_BELIEF_GOD_OF_MARTIAL_LAW_NAME', 'Gott der Götter'),
+('de_DE', 'LOC_BELIEF_GOD_OF_MARTIAL_LAW_DESC', '+1 Joker-Politikplatz, doppelte Ertäge des Palastes und +20% Produktion für Wunder.');
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('de_DE', 'LOC_BELIEF_GOD_OF_THE_HOLY_ENCAMPMENT_NAME', 'Gott des Heiligen Lagers'),
+('de_DE', 'LOC_BELIEF_GOD_OF_THE_HOLY_ENCAMPMENT_DESC', '+1 Joker-Politikplatz. Städte mit einem Lager-Bezirk erhalten doppelte Produktion für defensive Gebäude und doppelte Ertäge des Palastes.');
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('de_DE', 'LOC_BELIEF_GOD_OF_HOLY_SEA_NAME', 'Gott der Heiligen See'),
+('de_DE', 'LOC_BELIEF_GOD_OF_HOLY_SEA_DESCRIPTION', '+1 [ICON_Production] Produktion und +1 [ICON_Faith] Glauben durch Fischerboote. +30 Heilung durch Heilige-Stätte-Bezirke.');
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('de_DE', 'LOC_BELIEF_GOD_OF_SUPER_SEA_NAME', 'Gott des Supermeeres'),
+('de_DE', 'LOC_BELIEF_GOD_OF_SUPER_SEA_DESCRIPTION', '+2 [ICON_Production] Produktion, +1 [ICON_Faith] Glauben, +1 [ICON_Culture] Kultur und +1 [ICON_Science] Wissenschaft durch Fischerboote.');
