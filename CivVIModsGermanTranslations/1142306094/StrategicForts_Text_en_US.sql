2e3298f437cc098626adef9aec9844a437233cbc
H.Humpel <H.Humpel@gmx.de>
2022-01-02 16:46:22 +0100

    [FIX] Strategic Forts
    Added missing (dummy) translations (EXPANSIONIST_UNLOCK)

--- CivVIModsGermanTranslations/1142306094/StrategicForts_Text_en_US.sql
+++ CivVIModsGermanTranslations/1142306094/StrategicForts_Text_en_US.sql
@@ -6,3 +6,9 @@ VALUES ('en_US', 'LOC_UNIT_EXPANSIONIST_NAME', 'Expansionist');
 
 INSERT OR REPLACE INTO LocalizedText ( Language, Tag, Text )
 VALUES ('en_US', 'LOC_UNIT_EXPANSIONIST_DESCRIPTION', 'Ancient Era builder unit. Can construct a single Fort.');
+
+INSERT OR REPLACE INTO LocalizedText ( Language, Tag, Text )
+VALUES ('en_US', 'LOC_BUILDING_EXPANSIONIST_UNLOCK_NAME', 'Expansionist Unlock');
+
+INSERT OR REPLACE INTO LocalizedText ( Language, Tag, Text )
+VALUES ('en_US', 'LOC_BUILDING_EXPANSIONIST_UNLOCK_DESCRIPTION', 'Allows human players to create the Expansionist unit.');
