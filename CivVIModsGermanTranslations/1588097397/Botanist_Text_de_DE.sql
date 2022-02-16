f6748a570e2e0eae5dc2c8b652b6cb6e2800ce0c
H.Humpel <H.Humpel@gmx.de>
2022-02-17 00:59:09 +0100

    [IMPROVED] Botanist & Herdsman
    Added support for "Latin American Resources" mod (can be enabled with the "Let Me Fix That For You" mod)

--- CivVIModsGermanTranslations/1588097397/Botanist_Text_de_DE.sql
+++ CivVIModsGermanTranslations/1588097397/Botanist_Text_de_DE.sql
@@ -23,10 +23,10 @@ values
 	('RESOURCE_SILK'),
 	--Rise and Fall
 	('RESOURCE_OLIVES'),
-	--AOM Cannabis and Hemp	
+	--AOM Cannabis and Hemp
 	('RESOURCE_AOM_HEMP'),
 	('RESOURCE_AOM_CANNABIS'),
-	--Resourceful	
+	--Resourceful
 	('RESOURCE_POPPIES'),
 	('RESOURCE_SAKURA'),
 	('RESOURCE_SORGHUM'),
@@ -52,7 +52,12 @@ values
 	('RESOURCE_CVS_POMEGRANATES'),
 	('RESOURCE_P0K_PLUMS'),
 	('RESOURCE_P0K_MAPLE'),
-	('RESOURCE_P0K_PAPYRUS')
+	('RESOURCE_P0K_PAPYRUS'),
+	-- Latin American Resources
+	('RESOURCE_LEU_P0K_QUINOA'),
+	('RESOURCE_LEU_P0K_POTATOES'),
+	('RESOURCE_LEU_P0K_YERBAMATE'),
+	('RESOURCE_LEU_P0K_COCA')
 	;
 
 update Botanist_Resources
@@ -61,7 +66,7 @@ ImprovementType =  'IMPROVEMENT_TRANSPLANT_'|| replace(ResourceType, 'RESOURCE_'
 
 insert into LocalizedText
 (Tag,Language,Text)
-select 
+select
 'LOC_' || br.UnitType || '_NAME' as Tag,
 lt.Language,
 (case when instr(lt.Text, '|') > 0 then
@@ -75,7 +80,7 @@ join LocalizedText as lt on Tag = 'LOC_' ||br.ResourceType|| '_NAME';
 
 insert into LocalizedText
 (Tag,Language,Text)
-select 
+select
 'LOC_' || br.UnitType || '_DESCRIPTION' as Tag,
 lt.Language,
 (case when instr(lt.Text, '|') > 0 then
@@ -90,7 +95,7 @@ join LocalizedText as lt on Tag = 'LOC_' ||br.ResourceType|| '_NAME';
 
 insert into LocalizedText
 (Tag,Language,Text)
-select 
+select
 'LOC_' || br.ImprovementType || '_NAME' as Tag,
 lt.Language,
 (case when instr(lt.Text, '|') > 0 then
@@ -104,7 +109,7 @@ join LocalizedText as lt on Tag = 'LOC_' ||br.ResourceType|| '_NAME';
 
 insert into LocalizedText
 (Tag,Language,Text)
-select 
+select
 'LOC_' || br.ImprovementType || '_DESCRIPTION' as Tag,
 lt.Language,
 (case when instr(lt.Text, '|') > 0 then
@@ -117,21 +122,21 @@ join LocalizedText as lt on Tag = 'LOC_' ||br.ResourceType|| '_NAME';
 
 
 
-update LocalizedText 
-set Text = 'Botaniker (Maulbeersamen)' 
+update LocalizedText
+set Text = 'Botaniker (Maulbeersamen)'
 where Tag = 'LOC_UNIT_BOTANIST_SILK_NAME';
 
-update LocalizedText 
-set Text = 'Ein Botaniker mit Maulbeersamen.' 
+update LocalizedText
+set Text = 'Ein Botaniker mit Maulbeersamen.'
 where Tag = 'LOC_UNIT_BOTANIST_SILK_DESCRIPTION';
 
 
-update LocalizedText 
-set Text = 'Anpflanzen (Maulbeersamen)' 
+update LocalizedText
+set Text = 'Anpflanzen (Maulbeersamen)'
 where Tag = 'LOC_IMPROVEMENT_TRANSPLANT_SILK_NAME';
 
-update LocalizedText 
-set Text = 'Pflanzt Maulbeersamen (für Seidenraupen) auf einem geeigneten Feld an.' 
+update LocalizedText
+set Text = 'Pflanzt Maulbeersamen (für Seidenraupen) auf einem geeigneten Feld an.'
 where Tag = 'LOC_IMPROVEMENT_TRANSPLANT_SILK_DESCRIPTION';
 
 
