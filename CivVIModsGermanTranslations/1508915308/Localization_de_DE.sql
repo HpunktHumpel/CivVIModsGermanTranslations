670f031108192d1409b825c1c52ae3fcac5118b1
H.Humpel <H.Humpel@gmx.de>
2022-02-24 00:12:09 +0100

    More declinations added
    - [IMPROVED] Brandenburg Gate (World Wonder)
    - [IMPROVED] Buddhas of Bamyan (World Wonder)
    - [IMPROVED] Diversity City Center Buildings
    - [IMPROVED] Leaning Tower of Pisa (World Wonder)
    - [IMPROVED] SquirrelMod - BetterCity
    - [IMPROVED] Tide Mill Building
    - [IMPROVED] Trading Post Improvement

--- CivVIModsGermanTranslations/1508915308/Localization_de_DE.sql
+++ CivVIModsGermanTranslations/1508915308/Localization_de_DE.sql
@@ -1,5 +1,5 @@
-INSERT OR REPLACE INTO LocalizedText (Tag, Language, Text)
-VALUES ("LOC_BUILDING_SAILOR_TIDE_MILL_NAME", "de_DE", "Gezeitenmühle");
+INSERT OR REPLACE INTO LocalizedText (Tag, Language, Text, Gender, Plurality)
+VALUES ("LOC_BUILDING_SAILOR_TIDE_MILL_NAME", "de_DE", "Gezeitenmühle|Gezeitenmühle|Gezeitenmühle|Gezeitenmühle|Gezeitenmühle", "feminine", "1");
 
 INSERT OR REPLACE INTO LocalizedText (Tag, Language, Text)
 VALUES ("LOC_BUILDING_SAILOR_TIDE_MILL_DESCRIPTION", "de_DE", "[ICON_RESOURCE_IRON] Eisen-, [ICON_RESOURCE_MARBLE] Marmor- und [ICON_RESOURCE_STONE] Stein-Ressourcen in dieser Stadt erhalten jeweils +1 [ICON_Production] Produktion. Die Stadt muss an einer Küste liegen.");
--- CivVIModsGermanTranslations/2546679477/Texts_de_DE.sql
+++ CivVIModsGermanTranslations/2546679477/Texts_de_DE.sql
@@ -1,20 +1,25 @@
 --------------------------------------------------------------------------------
 -- Language: de_DE
 insert or replace into LocalizedText
+    (Language,	Tag, Text, Gender, Plurality)
+values
+    -- Gebäude im Stadtzentrum - Namen
+    ('de_DE',	'LOC_BUILDING_NILOMETER_HD_NAME',	'Nilometer|Nilometer|Nilometer|Nilometers|Nilometer', 'neuter', '1'),
+    ('de_DE',	'LOC_BUILDING_TRIUMPHAL_ARCH_NAME',	'Triumphbogen|Triumphbogen|Triumphbogen|Triumphbogens|Triumphbogen', 'masculine', '1'),
+    ('de_DE',	'LOC_BUILDING_OFFICIAL_RUN_HANDCRAFT_NAME',	'Handwerkerbetrieb|Handwerkerbetrieb|Handwerkerbetrieb|Handwerkerbetriebes|Handwerkerbetrieb', 'masculine', '1'),
+    ('de_DE',	'LOC_BUILDING_BOOTCAMP_NAME',	'Trainingslager|Trainingslager|Trainingslager|Trainingslagers|Trainingslager', 'neuter', '1'),
+    ('de_DE',	'LOC_BUILDING_FAIR_NAME',	'Jahrmarkt|Jahrmarkt|Jahrmarkt|Jahrmarktes|Jahrmarkt', 'masculine', '1'),
+    ('de_DE',	'LOC_BUILDING_TOTEMS_NAME',	'Totem|Totem|Totem|Totems|Totem', 'neuter', '1');
+
+insert or replace into LocalizedText
     (Language,	Tag,							Text)
 values
-    -- Gebäude im Stadtzentrum
-    ('de_DE',	'LOC_BUILDING_NILOMETER_HD_NAME',			'Nilometer'),
+    -- Gebäude im Stadtzentrum - Beschreibungen
     ('de_DE',	'LOC_BUILDING_NILOMETER_HD_DESCRIPTION',		'Bietet zusätzlich +1 [ICON_Science] Wissenschaft, wenn das Stadtzentrum auf einem Schwemmland-Geländefeld liegt.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Triumphbogen steht.'),
-    ('de_DE',	'LOC_BUILDING_TRIUMPHAL_ARCH_NAME',			'Triumphbogen'),
     ('de_DE',	'LOC_BUILDING_TRIUMPHAL_ARCH_DESCRIPTION',		'Bietet zusätzlich +1 [ICON_Culture] Kultur, wenn in der Stadt ein Weltwunder steht.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Nilometer steht.'),
-    ('de_DE',	'LOC_BUILDING_KAREZ_NAME',				'Karez'),
+    ('de_DE',	'LOC_BUILDING_KAREZ_NAME', 'Karez'),
     ('de_DE',	'LOC_BUILDING_KAREZ_DESCRIPTION',			'Bietet zusätzlich +1 [ICON_Food] Nahrung und +1 [ICON_Housing] Wohnraum, wenn das Stadtzentrum auf einem Hügel- oder neben einem Berg-Geländefeld liegt.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Handwerkerbetrieb oder ein Trainingslager steht.'),
-    ('de_DE',	'LOC_BUILDING_OFFICIAL_RUN_HANDCRAFT_NAME',		'Handwerkerbetrieb'),
     ('de_DE',	'LOC_BUILDING_OFFICIAL_RUN_HANDCRAFT_DESCRIPTION',	'Bietet zusätzlich +1 [ICON_Production] Produktion für Bezirke, Gebäude und Wunder in dieser Stadt, wenn die Stadt eine Mine oder einen Steinbruch auf einer Luxus- oder Bonus-Ressource besitzt.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Karez oder ein Trainingslager steht.'),
-    ('de_DE',	'LOC_BUILDING_BOOTCAMP_NAME',				'Trainingslager'),
     ('de_DE',	'LOC_BUILDING_BOOTCAMP_DESCRIPTION',			'Bietet zusätzlich +2 [ICON_Production] Produktion für Einheiten in dieser Stadt, wenn die Stadt eine Weide oder ein Jagdlager auf einer Ressource, oder eine Verbesserung auf einer strategischen Ressource besitzt.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Karez oder ein Handwerkerbetrieb steht.'),
-    ('de_DE',	'LOC_BUILDING_FAIR_NAME',				'Jahrmarkt'),
     ('de_DE',	'LOC_BUILDING_FAIR_DESCRIPTION',			'Bietet zusätzlich +2 [ICON_Gold] Gold, wenn das Stadtzentrum an mindestens eine Luxus-Ressource angrenzt.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Totem steht.'),
-    ('de_DE',	'LOC_BUILDING_TOTEMS_NAME',				'Totem'),
     ('de_DE',	'LOC_BUILDING_TOTEMS_DESCRIPTION',			'Bietet zusätzlich +1 [ICON_GreatProphet] Punkt des Typs "Großer Prophet", wenn das Stadtzentrum neben einem Berg- oder Wald-Geländefeld liegt.[NEWLINE][NEWLINE]Kann nicht in einer Stadt gebaut werden, in der schon ein Jahrmarkt steht.');
