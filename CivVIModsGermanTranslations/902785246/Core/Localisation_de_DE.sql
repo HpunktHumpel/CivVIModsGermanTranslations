7a12b467e6357f3fd0763341e557a74930b48ef3
H.Humpel <H.Humpel@gmx.de>
2022-07-25 16:51:30 +0200

    New mod added: Sukritact's Tonlé Sap

---
+++ CivVIModsGermanTranslations/902785246/Core/Localisation_de_DE.sql
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------------------------------------------------
+-- LocalizedText
+--------------------------------------------------------------------------------------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag, 																			Language,					Text)
+VALUES  
+--------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
+		("LOC_FEATURE_SUK_TONLESAP_NAME",  												"de_DE",
+		"Tonlé Sap"																),
+		("LOC_FEATURE_SUK_TONLESAP_DESCRIPTION",  										"de_DE",
+		"Naturwunder über zwei Felder. Erscheint als See und gewährt +3 [ICON_Food] Nahrung, +2 [ICON_Production] Produktion und Süßwasser. Handwerker, die sich neben Tonlé Sap bewegen erhalten eine zusätzliche Ladung."																),
+		("LOC_FEATURE_SUK_TONLESAP_QUOTE",  											"de_DE",
+		"“Während mehr als fünf Monaten im Jahr bedeckt der große See von Kambodscha, der Touli-Sap, eine riesige Fläche: Nach dieser Zeit nimmt die Tiefe aufgrund der großen Verdunstung ab, aber seine Breite bleibt fast unverändert.”[NEWLINE]– Henri Mouhot"																),
+		("LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_TONLESAP_CHAPTER_HISTORY_PARA_1",  		"de_DE",
+		"Tonlé Sap, gemeinhin mit 'Großer See' übersetzt, bezieht sich auf einen saisonal überfluteten Süßwassersee, den Tonlé-Sap-See, und einen angeschlossenen Fluss, den 120 km langen Tonlé-Sap-Fluss, der den See mit dem Mekong verbindet. Sie bilden den zentralen Teil eines komplexen hydrologischen Systems in der 12.876 km² großen kambodschanischen Flussaue, die mit einem Mosaik natürlicher und landwirtschaftlicher Lebensräume bedeckt ist, die der Mekong jährlich mit Wasser und Sedimenten auffüllt. Als eines der vielfältigsten und produktivsten Ökosysteme der Welt war die Region schon immer von zentraler Bedeutung für die Nahrungsmittelversorgung Kambodschas. Sie war in der Lage, die angkoreanische Zivilisation, den größten vorindustriellen Siedlungskomplex der Weltgeschichte, weitgehend zu erhalten. Als größter Süßwassersee Südostasiens und mit einer außergewöhnlich großen Vielfalt an miteinander verbundenen Ökoregionen mit einem hohen Grad an Biodiversität wurde er 1997 zum UNESCO-Biosphärenreservat erklärt."																);
+--------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
