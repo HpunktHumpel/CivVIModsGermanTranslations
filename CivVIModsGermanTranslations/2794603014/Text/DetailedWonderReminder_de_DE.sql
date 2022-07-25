252eb9b03ad79c03f3f7e6b62e7418c7bb62b822
H.Humpel <H.Humpel@gmx.de>
2022-07-25 15:48:09 +0200

    [UPDATE] Detailed Wonder Reminder Enhanced

--- CivVIModsGermanTranslations/2794603014/Text/DetailedWonderReminder_de_DE.sql
+++ CivVIModsGermanTranslations/2794603014/Text/DetailedWonderReminder_de_DE.sql
@@ -3,23 +3,23 @@
 -- DateCreated: 4/14/2022 20:06:55
 --------------------------------------------------------------
 
-INSERT INTO LocalizedText
+INSERT INTO LocalizedText 
 		(Language,	Tag,										Text)
 VALUES	('de_DE',	'LOC_KNM_REMINDER_BUILDING',				'{1_CivName} [ICON_Turn] {2_Number}'),
-		('de_DE',	'LOC_KNM_REMINDER_BUILT',					'Gebaut durch {1_CivName}'),
-		('de_DE',	'LOC_KNM_REMINDER_UNREVEAL',				'Standort unbekannt'),
-		('de_DE',	'LOC_KNM_REMINDER_BUILT_TOOLTIP',			'Erbaut in Runde {1_Turn} durch {2_CivName}.'),
-		('de_DE',	'LOC_KNM_REMINDER_TITLE',					'Wunder-Erinnerung'),
-		('de_DE',	'LOC_KNM_REMINDER_LAUNCH_NAME',				'Wunder-Erinnerung[NEWLINE][NEWLINE]Zeigt Bauinformationen für jedes Weltwunder an - für alle Zivilisationen, sowie für Eure Städte, die die Bauvoraussetzungen erfülle. Die Standard-Schnelltaste ist K, die in den Optionen geändert werden kann.'),
-		('de_DE',	'LOC_KNM_REMINDER_CHECK_BUILT',				'Gebaut'),
-		('de_DE',	'LOC_KNM_REMINDER_CHECK_TO_BUILD',			'Ungebaut'),
-		('de_DE',	'LOC_KNM_REMINDER_CITIES',					'Städte, die die Bauvoraussetzungen erfüllen:'),
-		('de_DE',	'LOC_KNM_REMINDER_CITY',					'{1_CityName}: Voraussichtlich[ICON_Turn] {2_Number}, produziert {3_String}'),
-		('de_DE',	'LOC_KNM_REMINDER_CITY_IDLE',				'{1_CityName}: Voraussichtlich[ICON_Turn] {2_Number}, inaktiv'),
+		('de_DE',	'LOC_KNM_REMINDER_BUILT',					'Erbaut durch {1_CivName}'),
+		('de_DE',	'LOC_KNM_REMINDER_UNREVEAL',				'Ort unbekannt'),
+		('de_DE',	'LOC_KNM_REMINDER_BUILT_TOOLTIP',			'In Runde {1_Turn}, erbaute es {2_CivName}.'),
+		('de_DE',	'LOC_KNM_REMINDER_TITLE',					'Weltwunder Gedächtnisstütze'),
+		('de_DE',	'LOC_KNM_REMINDER_LAUNCH_NAME',				'Weltwunder Gedächtnisstütze[NEWLINE][NEWLINE]Zeigt für jedes Wunder Informationen zum Bau und Städte mit passenden Voraussetzungen an. Das Tastaturkürzel ist standardmäßig K, kann aber in den Optionen geändert werden.'),
+		('de_DE',	'LOC_KNM_REMINDER_CHECK_BUILT',				'Erbaut'),
+		('de_DE',	'LOC_KNM_REMINDER_CHECK_TO_BUILD',			'Noch nicht errichtet'),
+		('de_DE',	'LOC_KNM_REMINDER_CITIES',					'Städte mit passenden Voraussetzungen:'),
+		('de_DE',	'LOC_KNM_REMINDER_CITY',					'{1_CityName}: Benötigt {2_Number}[ICON_Turn], baut {3_String}'),
+		('de_DE',	'LOC_KNM_REMINDER_CITY_IDLE',				'{1_CityName}: Benötigt {2_Number}[ICON_Turn], tut nichts'),
 		('de_DE',	'LOC_KNM_REMINDER_NATIONAL_WONDER',			'Nationales Wunder'),
-		('de_DE',	'LOC_KNM_REMINDER_CHECKBOX1_TT',			'Gebaute Wunder ein- oder ausblenden.[NEWLINE]Die Tastenkombination ist die Zahl "1" (Haupttastaturbereich).'),
-		('de_DE',	'LOC_KNM_REMINDER_CHECKBOX2_TT',			'Die im Bau befindlichen Wunder ein- oder ausblenden.[NEWLINE]Die Tastenkombination ist die Zahl "2" (Haupttastaturbereich).'),
-		('de_DE',	'LOC_KNM_REMINDER_CHECKBOX3_TT',			'Ungebaute Wunder ein- oder ausblenden.[NEWLINE]Die Tastenkombination ist die Zahl "3" (Haupttastaturbereich).'),
-		('de_DE',	'LOC_OPTIONS_HOTKEY_UI_KDWR',				'Wunder-Erinnerung umschalten'),
-		('de_DE',	'LOC_OPTIONS_HOTKEY_UI_KDWR_HELP',			'Schaltet die Sichtbarkeit der Wunder-Erinnerung um'),
-		('de_DE',	'LOC_NOTIFICATION_KNM_DWR_OTHER_START_BUILD_SUM',			'Eine andere Zivilisation hat mit dem Bau von {1_Wonder} begonnen und wird den Bau vor uns abschließen (in {2_Number}[ICON_Turn] Runden).');
+		('de_DE',	'LOC_KNM_REMINDER_CHECKBOX1_TT',			'Blende erbaute Wunder ein oder aus.[NEWLINE]Das Tastaturkürzel ist Nummer "1" (Tastatur-Hauptbereich).'),
+		('de_DE',	'LOC_KNM_REMINDER_CHECKBOX2_TT',			'Blende im Bau befindliche Wunder ein oder aus.[NEWLINE]Das Tastaturkürzel ist Nummer "2" (Tastatur-Hauptbereich).'),
+		('de_DE',	'LOC_KNM_REMINDER_CHECKBOX3_TT',			'Blende nicht errichtete Wunder ein oder aus.[NEWLINE]Das Tastaturkürzel ist Nummer "3" (Tastatur-Hauptbereich).'),
+		('de_DE',	'LOC_OPTIONS_HOTKEY_UI_KDWR',				'Ruft Weltwunder Gedächtnisstütze auf'),
+		('de_DE',	'LOC_OPTIONS_HOTKEY_UI_KDWR_HELP',			'Schaltet die Sichtbarkeit der Weltwunder Gedächtnisstütze um'),
+		('de_DE',	'LOC_NOTIFICATION_KNM_DWR_OTHER_START_BUILD_SUM',			'Eine weitere Nation beginnt mit dem Bau des Wunders {1_Wonder} und stellt es vor uns fertig (in {2_Number}[ICON_Turn] Runden).');
