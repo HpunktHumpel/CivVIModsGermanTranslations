a437988aaffc267b149f60c01157e47e50c3817f
H.Humpel <H.Humpel@gmx.de>
2022-11-14 17:13:34 +0100

    [UPDATE] Policy Pack: Corporations

--- CivVIModsGermanTranslations/2857099961/Core/PP_Corporations_Text_de_DE.sql
+++ CivVIModsGermanTranslations/2857099961/Core/PP_Corporations_Text_de_DE.sql
@@ -12,7 +12,8 @@ VALUES	('de_DE',	'LOC_POLICY_JNR_DUAL_USE_TECH_NAME',			'Dual-Use-Technologien|D
 		('de_DE',	'LOC_POLICY_JNR_SELF_MANAGEMENT_NAME',			'Arbeiterselbstverwaltung|Arbeiterselbstverwaltung|Arbeiterselbstverwaltung|Arbeiterselbstverwaltung|Arbeiterselbstverwaltung', 'feminine', 1),
 		('de_DE',	'LOC_POLICY_JNR_FORCED_LABOR_NAME',			'Zwangsarbeit|Zwangsarbeit|Zwangsarbeit|Zwangsarbeit|Zwangsarbeit', 'feminine', 1),
 		('de_DE',	'LOC_POLICY_JNR_JOINT_VENTURES_NAME',			'Gemeinschaftsunternehmen|Gemeinschaftsunternehmen|Gemeinschaftsunternehmen|Gemeinschaftsunternehmens|Gemeinschaftsunternehmen', 'neuter', 1),
-		('de_DE',	'LOC_POLICY_JNR_PHILANTHROPY_NAME',			'Philanthropie|Philanthropie|Philanthropie|Philanthropie|Philanthropie', 'feminine', 1);
+		('de_DE',	'LOC_POLICY_JNR_PHILANTHROPY_NAME',			'Philanthropie|Philanthropie|Philanthropie|Philanthropie|Philanthropie', 'feminine', 1),
+		('de_DE', 'LOC_POLICY_JNR_TRIPLE_BOTTOM_LINE_NAME',			'Drei-Säulen-Modell|Drei-Säulen-Modell|Drei-Säulen-Modell|Drei-Säulen-Modells|Drei-Säulen-Modell', 'neuter', 1);
 
 INSERT OR REPLACE INTO LocalizedText
 		(Language,	Tag,											Text)
@@ -22,5 +23,6 @@ VALUES	('de_DE',	'LOC_POLICY_JNR_DUAL_USE_TECH_DESCRIPTION',	'+50% [ICON_Product
 		('de_DE',	'LOC_POLICY_JNR_SELF_MANAGEMENT_DESCRIPTION',	'Industrien erhalten +4 [ICON_Production] Produktion und +4 [ICON_Culture] Kultur.'),
 		('de_DE',	'LOC_POLICY_JNR_FORCED_LABOR_DESCRIPTION',		'Korporationen erhalten +1 [ICON_Production] Produktion für jede kontrollierte fremdgegründete Stadt.'),
 		('de_DE',	'LOC_POLICY_JNR_JOINT_VENTURES_DESCRIPTION',	'Internationale [ICON_TradeRoute] Handelswege aus Städten innerhalb von 3 Geländefeldern zu einer Korporation bieten +2 [ICON_Science] Wissenschaft.'),
-		('de_DE',	'LOC_POLICY_JNR_PHILANTHROPY_DESCRIPTION',		'An eine Industrie angrenzende Industriegebiete bieten +1 ihrer [ICON_GreatPerson] Großen Persönlichkeiten-Punkte. Verdoppelt für Korporationen.');
+		('de_DE',	'LOC_POLICY_JNR_PHILANTHROPY_DESCRIPTION',		'An eine Industrie angrenzende Industriegebiete bieten +1 ihrer [ICON_GreatPerson] Großen Persönlichkeiten-Punkte. Verdoppelt für Korporationen.'),
+		('de_DE',	'LOC_POLICY_JNR_TRIPLE_BOTTOM_LINE_DESCRIPTION',	'Je +2 [ICON_GreatScientist] "Großer Wissenschaftler"-, [ICON_GreatEngineer] "Großer Ingenieur"- und [ICON_GreatMerchant] "Großer Händler"-Punkte für jedes Gebäude in einem Schutzgebiet-Bezirk in begeisterten Städten innerhalb von 3 Geländefeldern zu einer Korporation.');
 --------------------------------------------------------------
---
+++ Originals/2857099961 - Policy Pack Corporations/Core/PP_Corporations_Text.sql
@@ -0,0 +1,25 @@
+-- PP_Corporations_Text
+-- Author: JNR
+--------------------------------------------------------------
+
+-- English
+--------------------------------------------------------------
+INSERT OR IGNORE INTO EnglishText
+		(Tag,												Text)
+VALUES	('LOC_POLICY_JNR_DUAL_USE_TECH_NAME',				'Dual-Use Technologies'),
+		('LOC_POLICY_JNR_DUAL_USE_TECH_DESCRIPTION',		'+50% [ICON_Production] Production to Create New Product projects.'),
+		('LOC_POLICY_JNR_QFD_NAME',							'Quality Function Deployment'),
+		('LOC_POLICY_JNR_QFD_DESCRIPTION',					'[ICON_GreatWork_Product] Products have triple yields.'),
+		('LOC_POLICY_JNR_FLAGSHIP_STORES_NAME',				'Flagship Stores'),
+		('LOC_POLICY_JNR_FLAGSHIP_STORES_DESCRIPTION',		'[ICON_GreatWork_Product] Products have decuple (x10) [ICON_Tourism] Tourism.'),
+		('LOC_POLICY_JNR_SELF_MANAGEMENT_NAME',				'Workers Self-Management'),
+		('LOC_POLICY_JNR_SELF_MANAGEMENT_DESCRIPTION',		'Industries gain +4 [ICON_Production] Production and +4 [ICON_Culture] Culture.'),
+		('LOC_POLICY_JNR_FORCED_LABOR_NAME',				'Forced Labor'),
+		('LOC_POLICY_JNR_FORCED_LABOR_DESCRIPTION',			'Corporations gain +1 [ICON_Production] Production for every controlled foreign-founded city.'),
+		('LOC_POLICY_JNR_JOINT_VENTURES_NAME',				'Joint Ventures'),
+		('LOC_POLICY_JNR_JOINT_VENTURES_DESCRIPTION',		'International [ICON_TradeRoute] Trade Routes from cities within 3 tiles of a Corporation provide +2 [ICON_Science] Science.'),
+		('LOC_POLICY_JNR_PHILANTHROPY_NAME',				'Philanthropy'),
+		('LOC_POLICY_JNR_PHILANTHROPY_DESCRIPTION',			'Districts adjacent to an Industry provide +1 of their respective [ICON_GreatPerson] Great Person point. Doubled for Corporations.'),
+		('LOC_POLICY_JNR_TRIPLE_BOTTOM_LINE_NAME',			'Triple Bottom Line'),
+		('LOC_POLICY_JNR_TRIPLE_BOTTOM_LINE_DESCRIPTION',	'+2 [ICON_GreatScientist] Great Scientist, [ICON_GreatEngineer] Engineer, and [ICON_GreatMerchant] Merchant points per Preserve building in ecstatic cities within 3 tiles of a Corporation.');
+--------------------------------------------------------------
