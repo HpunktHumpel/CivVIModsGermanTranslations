71388b200cd429969a7f6effe311a13af7ac6bb3
H.Humpel <H.Humpel@gmx.de>
2022-11-20 16:07:24 +0100

    [IMPROVED] Requested Pantheons

---
+++ Originals/1888611021 - Requested Pantheons/Pantheons_Text.sql
@@ -0,0 +1,25 @@
+--------------------------------------------------------------
+-- Pantheons_Text
+-- Author: Angryr
+-- DateCreated: 10/13/2019 11:52:41 AM
+--------------------------------------------------------------
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('en_US', 'LOC_BELIEF_GOD_OF_MARTIAL_LAW_NAME', 'God of the Gods'),
+('en_US', 'LOC_BELIEF_GOD_OF_MARTIAL_LAW_DESC', '+1 Wildcard slot, double palace yields, and +20% production towards wonders.');
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('en_US', 'LOC_BELIEF_GOD_OF_THE_HOLY_ENCAMPMENT_NAME', 'God of the Holy Encampment'),
+('en_US', 'LOC_BELIEF_GOD_OF_THE_HOLY_ENCAMPMENT_DESC', '+1 Wildcard slot. Cities with an encampment gain double wall production and double palace yields.');
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('en_US', 'LOC_BELIEF_GOD_OF_HOLY_SEA_NAME', 'God of the Holy Sea'),
+('en_US', 'LOC_BELIEF_GOD_OF_HOLY_SEA_DESCRIPTION', '+1 [ICON_Production] and +1 [ICON_Faith] from Fishing Boats. +30 Healing from Holy Sites.');
+
+INSERT INTO LocalizedText (Language, Tag, Text)
+VALUES
+('en_US', 'LOC_BELIEF_GOD_OF_SUPER_SEA_NAME', 'God of the Super Sea'),
+('en_US', 'LOC_BELIEF_GOD_OF_SUPER_SEA_DESCRIPTION', '+2 [ICON_Production], +1 [ICON_Faith], +1 [ICON_Culture], and +1 [ICON_Science] from Fishing Boats.');
