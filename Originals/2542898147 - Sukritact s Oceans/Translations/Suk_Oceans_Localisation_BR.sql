ca29d95bfb1f1d5bf89c73bfb17fa8ffea09c09b
H.Humpel <H.Humpel@gmx.de>
2022-11-21 00:06:17 +0100

    Updated Originals

---
+++ Originals/2542898147 - Sukritact s Oceans/Translations/Suk_Oceans_Localisation_BR.sql
@@ -0,0 +1,155 @@
+--========================================================================================================================
+-- LocalizedText
+--========================================================================================================================
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,								Language,		Text)
+VALUES
+--========================================================================================================================
+-- Game Mode
+--========================================================================================================================
+		(
+			"LOC_GAMEMODE_SUK_OCEANS_NAME",
+			"pt_BR",
+			"Oceanos por Sukritact"
+		),
+		(
+			"LOC_GAMEMODE_SUK_OCEANS_DESCRIPTION",
+			"pt_BR",
+			"Este modo de jogo dar nova vida aos oceanos do mundo, com novos recursos, novas características e nova jogabilidade.[NEWLINE][NEWLINE]Os bosques de algas ocupam as costas, fomentando o crescimento e desacelerando suas unidades. Novos recursos, como focas, lulas e corais, aguardam abaixo nas profundidades, adicionando mais variedade. Os recursos de luxo costeiros são agora diferentes para cada continente, pois os novos oceanos produzirão novas riquezas. As melhorias e os distritos, como o barco de pesca e o parque aquático, são ajustados levando em conta essas mudanças."
+		),
+--========================================================================================================================
+-- Squid
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_SQUID_NAME",
+			"pt_BR",
+			"Lula"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SQUID_CHAPTER_HISTORY_PARA_1",
+			"pt_BR",
+			"As lulas pertencem a um grupo de moluscos especialmente bem-sucedido chamado cefalópodes, que existem desde 500 milhões de anos. As lulas se encontram em oceanos de todo o mundo. Algumas lulas preferem as aguas tropicais mais calorosas, enquanto outras florescem em mares mais frios. As lulas são nadadoras rápidas, se movendo por propulsão a jato e localizam suas presas em grandes distâncias. Se encontram entre os invertebrados mais inteligentes, e já foi observado grupos de lulas Humboldt caçando de forma cooperativa. são presas de tubarões, outros peixes, aves marinhas, focas e cetáceos."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SQUID_CHAPTER_HISTORY_PARA_2",
+			"pt_BR",
+			"Lulas constituem um importante recurso alimentar e são utilizadas nas cozinhas de todo o mundo. Em países de língua inglesa, a lula como alimento é frequentemente chamada de calamar, adotada do italiano para o inglês no século 17. Lulas são encontradas em abundância em certas zonas e fornecem grandes capturas para a pesca. O corpo pode ser recheado inteiro, cortado em pedaços planos ou em anéis. Os braços, tentáculos e tinta também são comestíveis; as únicas partes que não são comidas são o bico e o gládio. A lula é uma boa fonte de zinco e manganês e é rica em cobre, selênio, vitamina B12 e riboflavina."
+		),
+--========================================================================================================================
+-- Seals
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_SEALS_NAME",
+			"pt_BR",
+			"Focas"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SEALS_CHAPTER_HISTORY_PARA_1",
+			"pt_BR",
+			"Existem atualmente 33 espécies de pinípedes, a maioria das quais são conhecidas como focas. Os pinípedes podem ser encontrados em todos os continentes da Terra, embora a maioria das espécies sejam encontradas em ambientes de água fria. Embora existam muitas diferenças entre as espécies, todas as focas têm pés em forma de barbatana. Na verdade, a palavra pinípede significa “pés com nadadeiras” em latim. O tamanho das focas varia muito, desde o gigantesco elefante marinho do sul, que pode pesar mais do que uma caminhonete, até a relativamente fina foca do Baikal, de 45 quilos."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SEALS_CHAPTER_HISTORY_PARA_2",
+			"pt_BR",
+			"Historicamente, os caçadores perseguiram intensamente espécies de pinípedes por causa de sua pele, uma prática que levou algumas espécies à extinção: a foca-monge caribenha, por exemplo, desapareceu do planeta na década de 1970. Embora a caça seja mais regulamentada hoje, os animais continuam enfrentando muitas ameaças, como falta de comida, emaranhamento em equipamentos de pesca e conflitos com os pescadores. Algumas espécies, como a foca-monge havaiana e a foca-monge do Mediterrâneo, permanecem muito perto da extinção. No entanto, a mudança climática representa a maior ameaça para muitas espécies de pinípedes, especialmente aqueles que dependem do gelo marinho."
+		),
+--========================================================================================================================
+-- Coral
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_CORAL_NAME",
+			"pt_BR",
+			"Coral"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CORAL_CHAPTER_HISTORY_PARA_1",
+			"pt_BR",
+			"O mar e a praia nunca deixaram de ser uma inspiração sem fim para os artistas. Vermelho, rosa, laranja e preto são algumas das cores de coral mais procuradas em joalherias. O coral colhido é geralmente cortado e polido em cabochões, moldado em contas ou esculpido. A parte do coral que é usada para joalheria não é propriamente o organismo vivo, mas suas secreções de carbonato, que formam a estrutura na qual vivem os pólipos, e embora não seja a parte viva do animal, é essencial para sua sobrevivência."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CORAL_CHAPTER_HISTORY_PARA_2",
+			"pt_BR",
+			"Hoje, o uso de coral em joias é polêmico. As joias de coral continuam populares e as peças antigas alcançam preços astronômicos nos leilões. Embora alguns joalheiros continuem a usar coral em seus designs, muitos deles usam apenas coral de peças antigas e outros pararam usar coral em geral. Com menos joalheiros usando coral, os recifes terão uma chance melhor de se recuperar. E, até então, os amantes de corais podem comprar belas peças de joalheria de coral antigo ou, se tiverem sorte, ver corais vivendo na natureza.."
+		),
+--========================================================================================================================
+-- Lobster
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_LOBSTER_NAME",
+			"pt_BR",
+			"Lagostas"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_LOBSTER_CHAPTER_HISTORY_PARA_1",
+			"pt_BR",
+			"As lagostas são uma família de grandes crustáceos marinhos. Eles têm corpos longos com caudas musculosas e vivem em fendas ou tocas no fundo do mar. Três de seus cinco pares de pernas têm garras, incluindo o primeiro par, que geralmente é muito maior do que os outros. Muito apreciada como marisco, a lagosta é importante do ponto de vista económico e costuma ser um dos produtos mais rentáveis ​​das zonas costeiras que povoam."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_LOBSTER_CHAPTER_HISTORY_PARA_2",
+			"pt_BR",
+			"A lagosta é geralmente servida cozida ou ao vapor com a casca. Os comensais quebram a casca com biscoitos de lagosta e pescam a carne com garras de lagosta. A carne é geralmente comida com manteiga derretida e suco de limão. A lagosta também é usada em sopas, bisque, rolos de lagosta, capão magro e pratos como Lobster Newberg e Lobster Thermidor."
+		),
+--========================================================================================================================
+-- Caviar
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_CAVIAR_NAME",
+			"pt_BR",
+			"Caviar"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CAVIAR_CHAPTER_HISTORY_PARA_1",
+			"pt_BR",
+			"Se existe um alimento associado ao puro luxo, é o caviar. Esta iguaria de ova de esturjão é rara e cara e considerada um item cobiçado na culinária mundial. O caviar vem de várias espécies de esturjão, mas o caviar beluga é o maior, o mais raro e o mais caro. Com um preço próximo a US $ 3.500 por libra, ganhou o apelido de 'ouro negro'. Dependendo do país, o caviar também pode ser usado para designar as ovas de outros peixes, como salmão, truta, baiacu, peixe branco ou carpa."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CAVIAR_CHAPTER_HISTORY_PARA_2",
+			"pt_BR",
+			"As populações de esturjão diminuíram em todo o mundo, em parte devido à popularidade do caviar como um alimento de luxo, e limites rígidos de captura ou mesmo proibições foram estabelecidos em muitos lugares. Outras razões para o declínio da população são a poluição da água e a perda de habitat das represas dos rios que impedem os peixes de chegarem aos locais de desova. Muitas espécies de esturjão estão na lista de espécies ameaçadas de extinção. Para aliviar a pressão sobre as populações cada vez menores de esturjão selvagem, empresas em 20 países criam esturjões. Na grande maioria dos casos, os ovos são removidos após matar os peixes. Outras empresas tentaram uma abordagem mais durável, criando os peixes até que estejam prontos para desovar e, depois de retirar os ovos por cesariana, costurá-los e criá-los para continuar a produzir. O processo é muito duro para os peixes e já foi proibido em vários países."
+		),
+--========================================================================================================================
+-- Kelp Forest
+--========================================================================================================================
+		(
+			"LOC_FEATURE_SUK_KELP_NAME",
+			"pt_BR",
+			"Floresta de Algas"
+		),
+		(
+			"LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_KELP_CHAPTER_HISTORY_PARA_1",
+			"pt_BR",
+			"As algas são grandes algas marrons que vivem em águas frias e relativamente rasas perto da costa. Eles crescem em aglomerados densos, como uma floresta em terra. Essas florestas têm uma maior variedade e densidade de plantas e animais do que quase qualquer outra comunidade oceânica; Essas florestas em camadas criam um habitat atraente para uma diversidade de espécies, servindo como fonte de alimento e refúgio para milhares de peixes, invertebrados e espécies de mamíferos marinhos."
+		),
+--========================================================================================================================
+-- Water Park Changes
+--========================================================================================================================
+		(
+			"LOC_DISTRICT_WATER_ENTERTAINMENT_COMPLEX_DESCRIPTION",
+			"pt_BR",
+			"Um distrito colocado em um painel costeiro dedicado a manter seu povo feliz aumentando [ICON_Amenities] Serviços. Se construído adjacente a uma Floresta de Algas, +1 de [ICON_Amenities] Serviços. Não pode ser construído em uma cidade com um Complexo de Entretenimento. Não pode ser construído em recife."
+		),
+		(
+			"LOC_BUILDING_AQUARIUM_DESCRIPTION",
+			"pt_BR",
+			"Bônus de [ICON_Amenities] serviço é estendido para cada centro da cidade dentro de 9 painéis de distância. Este bônus é aplicado uma vez em uma cidade, e várias cópias desta edificação dentro de 9 painéis de distância do centro de uma cidade não oferecem bônus adicional. +1 de [ICON_Science] ciência para cada painel de recurso litorâneo, naufrágio, floresta de algas e recife nesta cidade. Mais um +1 de [ICON_Science] Ciência a esses recursos se estão adjacentes ao Parque Aquático."
+		),
+		(
+			"LOC_BUILDING_AQUATICS_CENTER_DESCRIPTION",
+			"pt_BR",
+			"Bônus de [ICON_Amenities] serviço é estendido para cada centro da cidade dentro de 9 painéis de distância. Este bônus é aplicado uma vez em uma cidade, e várias cópias desta edificação dentro de 9 painéis de distância do centro de uma cidade não oferecem bônus adicional. Recebe +2 [ICON_Tourism] Turismo para cada característica costeira adjacente."
+		),
+--========================================================================================================================
+-- Fishery Changes
+--========================================================================================================================
+		(
+			"LOC_GOVERNOR_PROMOTION_AQUACULTURE_DESCRIPTION",
+			"pt_BR",
+			"Pode construir a melhoria de Aquicultura. +1 de [ICON_Housing] Habitação. Barcos de Pesca adjacentes produzem +2 de [ICON_Food] Alimentos; e mais +1 de [ICON_Production] Produção se Liang estiver na cidade."
+		),
+		(
+			"LOC_IMPROVEMENT_FISHERY_DESCRIPTION",
+			"pt_BR",
+			"A melhoria exclusiva de [ICON_Governor] Governador que pode ser construída em uma cidade com uma Governadora Topógrafa com a promoção de Aqucultura.[NEWLINE][NEWLINE] Não pode ser trabalhada. Barcos de pesca adjacentes produzem +2 de [ICON_Food] Alimentos; e mais +1 de [ICON_Production] Produção se Liang estiver na cidade. +1 de [ICON_Housing] Habitação. Deve ser construído em terreno costeiro adjacente a um recurso marítimo."
+		);
+--========================================================================================================================
+--========================================================================================================================
---
+++ Originals/2542898147 - Sukritact s Oceans/Translations/Suk_Oceans_Localisation_ES.sql
@@ -0,0 +1,155 @@
+--========================================================================================================================
+-- LocalizedText
+--========================================================================================================================
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,								Language,		Text)
+VALUES
+--========================================================================================================================
+-- Game Mode
+--========================================================================================================================
+		(
+			"LOC_GAMEMODE_SUK_OCEANS_NAME",
+			"es_ES",
+			"Sukritact’s Océanos"
+		),
+		(
+			"LOC_GAMEMODE_SUK_OCEANS_DESCRIPTION",
+			"es_ES",
+			"Este modo de juego da nueva vida a los océanos del mundo, con nuevos recursos, nuevas características y nueva jugabilidad.[NEWLINE][NEWLINE]Los bosques de algas llenan las costas, fomentando el crecimiento y ralentizando las armadas. Nuevos recursos, como focas, calamares y corales, aguardan ahora bajo las profundidades, añadiendo más variedad. Los lujos costeros son ahora diferentes para cada continente, por lo que los nuevos océanos producirán nuevas riquezas. Las mejoras y los distritos, como la pesquería y el parque acuático, se han ajustado para tener en cuenta estos cambios."
+		),
+--========================================================================================================================
+-- Squid
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_SQUID_NAME",
+			"es_ES",
+			"Calamar"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SQUID_CHAPTER_HISTORY_PARA_1",
+			"es_ES",
+			"Los calamares pertenecen a un grupo de moluscos especialmente exitosos llamados cefalópodos, que existen desde hace unos 500 millones de años. Los calamares se encuentran en los océanos de todo el mundo. Algunos calamares prefieren las aguas tropicales más cálidas, mientras que otros prosperan en los mares fríos. Los calamares son nadadores rápidos, se mueven por propulsión a chorro y localizan sus presas en gran medida por la vista. Se encuentran entre los invertebrados más inteligentes, y se han observado grupos de calamares de Humboldt cazando en cooperación. Son presa de tiburones, otros peces, aves marinas, focas y cetáceos."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SQUID_CHAPTER_HISTORY_PARA_2",
+			"es_ES",
+			"Los calamares constituyen un importante recurso alimentario y se utilizan en las cocinas de todo el mundo. En los países de habla inglesa, el calamar como alimento suele llamarse calamari, adoptado del italiano al inglés en el siglo XVII. Los calamares se encuentran en abundancia en ciertas zonas y proporcionan grandes capturas para la pesca. El cuerpo puede rellenarse entero, cortarse en trozos planos o cortarse en anillos. Los brazos, los tentáculos y la tinta también son comestibles; las únicas partes que no se comen son el pico y el gladius. El calamar es una buena fuente de zinc y manganeso, y tiene un alto contenido de cobre, selenio, vitamina B12 y riboflavina."
+		),
+--========================================================================================================================
+-- Seals
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_SEALS_NAME",
+			"es_ES",
+			"Focas"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SEALS_CHAPTER_HISTORY_PARA_1",
+			"es_ES",
+			"En la actualidad existen 33 especies de pinnípedos, la mayoría de las cuales se conocen como focas. Los pinnípedos pueden encontrarse en todos los continentes de la Tierra, aunque la mayoría de las especies se encuentran en entornos de aguas frías. Aunque hay muchas diferencias entre las especies, todas las focas tienen pies en forma de aletas. De hecho, la palabra pinnípedo significa “pies con aletas” en latín. El tamaño de las focas varía mucho, desde el gigantesco elefante marino del sur, que puede pesar más que una camioneta, hasta la foca del Baikal, relativamente delgada y de 45 kilos."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_SEALS_CHAPTER_HISTORY_PARA_2",
+			"es_ES",
+			"Históricamente, los cazadores han perseguido intensamente a las especies de pinnípedos por su piel, una práctica que llevó a algunas especies a la extinción: La foca monje del Caribe, por ejemplo, desapareció del planeta en la década de 1970. Aunque la caza está más regulada hoy en día, los animales siguen enfrentándose a muchas amenazas, como la falta de alimento, los enredos en las artes de pesca y los conflictos con los pescadores. Algunas especies, como la foca monje de Hawai y la foca monje del Mediterráneo, siguen estando muy cerca de la extinción. Sin embargo, el cambio climático representa la mayor amenaza para muchas especies de pinnípedos, especialmente las que dependen del hielo marino."
+		),
+--========================================================================================================================
+-- Coral
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_CORAL_NAME",
+			"es_ES",
+			"Coral"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CORAL_CHAPTER_HISTORY_PARA_1",
+			"es_ES",
+			"El océano y la playa nunca han dejado de proporcionar un sinfín de inspiración a los artistas. El rojo, el rosa, el naranja y el negro son algunos de los colores de coral más buscados en joyería. El coral recolectado se suele cortar y pulir en cabujones, se le da forma de cuentas o se talla. La parte del coral que se utiliza para la joyería no es en realidad el organismo vivo, sino sus secreciones de carbonato, que forman la estructura sobre la que viven los pólipos, y aunque no es la parte viva del animal, es esencial para su supervivencia."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CORAL_CHAPTER_HISTORY_PARA_2",
+			"es_ES",
+			"Hoy en día, el uso del coral en la joyería es controvertido. Las joyas de coral siguen siendo populares y las piezas antiguas alcanzan precios astronómicos en las subastas. Aunque algunos joyeros siguen utilizando el coral en sus diseños, muchos de ellos sólo utilizan coral procedente de piezas antiguas y otros han dejado de crear joyas de coral. Con menos joyeros que utilicen coral, los arrecifes tendrán más posibilidades de recuperarse. Y, hasta entonces, los amantes del coral pueden comprar hermosas piezas de joyería de coral antiguas, o si tienen suerte, ver corales viviendo en la naturaleza."
+		),
+--========================================================================================================================
+-- Lobster
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_LOBSTER_NAME",
+			"es_ES",
+			"Langostas"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_LOBSTER_CHAPTER_HISTORY_PARA_1",
+			"es_ES",
+			"Las langostas son una familia de grandes crustáceos marinos. Tienen cuerpos largos con colas musculosas y viven en grietas o madrigueras en el fondo del mar. Tres de sus cinco pares de patas tienen pinzas, incluido el primer par, que suelen ser mucho más grandes que los demás. Muy apreciadas como marisco, las langostas son importantes desde el punto de vista económico y suelen ser uno de los productos más rentables en las zonas costeras que pueblan."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_LOBSTER_CHAPTER_HISTORY_PARA_2",
+			"es_ES",
+			"El bogavante se suele servir hervido o al vapor con el caparazón. Los comensales rompen el caparazón con galletas de langosta y pescan la carne con pinzas de langosta. La carne suele comerse con mantequilla derretida y zumo de limón. El bogavante también se utiliza en la sopa, el bisque, los rollitos de bogavante, el cappon magro y platos como el Newberg de bogavante y el Thermidor de bogavante."
+		),
+--========================================================================================================================
+-- Caviar
+--========================================================================================================================
+		(
+			"LOC_RESOURCE_SUK_CAVIAR_NAME",
+			"es_ES",
+			"Caviar"
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CAVIAR_CHAPTER_HISTORY_PARA_1",
+			"es_ES",
+			"Si hay un alimento que se asocia con el lujo puro, es el caviar. Este manjar de huevas de esturión es raro y caro y se considera un artículo codiciado en el mundo culinario. El caviar procede de varias especies de esturión, pero el caviar de beluga es el más grande, el más raro y el más caro. Con un precio cercano a los 3.500 dólares por libra, merece su apodo de “oro negro”. Según el país, el caviar también puede utilizarse para designar las huevas de otros peces, como el salmón, la trucha, el pez globo, el pez blanco o la carpa."
+		),
+		(
+			"LOC_PEDIA_RESOURCES_PAGE_RESOURCE_SUK_CAVIAR_CHAPTER_HISTORY_PARA_2",
+			"es_ES",
+			"Las poblaciones de esturión han disminuido en todo el mundo, debido en parte a la popularidad del caviar como alimento de lujo, y en muchos lugares se han establecido límites estrictos de captura o incluso prohibiciones. Otras razones del declive de la población son la contaminación del agua y la pérdida de hábitat por las presas de los ríos que impiden a los peces llegar a sus zonas de desove. Muchas especies de esturión están en la lista de especies en peligro de extinción. Para aliviar la presión sobre las menguantes poblaciones de esturión silvestre, hay empresas en 20 países que crían esturiones. En la gran mayoría de los casos, los huevos se extraen después de matar a los peces. Otras empresas han intentado un enfoque más duradero, criando a los peces hasta que están listos para desovar, y luego, tras extraer los huevos mediante una cesárea, coserlos de nuevo y criarlos para que sigan produciendo. El proceso es muy duro para los peces y se ha prohibido en varios países."
+		),
+--========================================================================================================================
+-- Kelp Forest
+--========================================================================================================================
+		(
+			"LOC_FEATURE_SUK_KELP_NAME",
+			"es_ES",
+			"Bosque de algas"
+		),
+		(
+			"LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_KELP_CHAPTER_HISTORY_PARA_1",
+			"es_ES",
+			"Las algas son grandes algas marrones que viven en aguas frías y relativamente poco profundas cerca de la costa. Crecen en agrupaciones densas muy parecidas a un bosque en tierra. Estos bosques tienen una mayor variedad y densidad de plantas y animales que casi cualquier otra comunidad oceánica; estos bosques escalonados crean un hábitat atractivo para una diversidad de especies, sirviendo tanto de fuente de alimento como de refugio para miles de peces, invertebrados y especies de mamíferos marinos."
+		),
+--========================================================================================================================
+-- Water Park Changes
+--========================================================================================================================
+		(
+			"LOC_DISTRICT_WATER_ENTERTAINMENT_COMPLEX_DESCRIPTION",
+			"es_ES",
+			"Un distrito colocado en una baldosa costera dedicado a mantener a tu gente feliz incrementando [ICON_Amenities] Servicios. Si se construye adyacente a un Bosque de Algas, +1 [ICON_Amenities] de servicios. No puede ser construido en una ciudad con un Complejo de Entretenimiento. No se puede construir en un Arrecife."
+		),
+		(
+			"LOC_BUILDING_AQUARIUM_DESCRIPTION",
+			"es_ES",
+			"La bonificación [ICON_Amenities] se extiende a cada centro de la ciudad en un radio de 9 casillas. Esta bonificación se aplica una vez a una ciudad, y varias copias de este edificio en un radio de 9 casillas del centro de la ciudad no proporcionan bonificaciones adicionales. +1 [ICON_Science] Ciencia a cada ficha de Arrecife, Bosque de Algas o Naufragio de esta ciudad. Un +1 [ICON_Science] adicional de Ciencia a ellas si están adyacentes al Parque Acuático."
+		),
+		(
+			"LOC_BUILDING_AQUATICS_CENTER_DESCRIPTION",
+			"es_ES",
+			"La bonificación [ICON_Amenities] se extiende a cada centro de ciudad en un radio de 9 casillas. Esta bonificación se aplica una vez a una ciudad, y varias copias de este edificio en un radio de 9 casillas del centro de una ciudad no proporcionan bonificaciones adicionales. Recibe +2 [ICON_Tourism] Turismo por cada rasgo costero adyacente."
+		),
+--========================================================================================================================
+-- Fishery Changes
+--========================================================================================================================
+		(
+			"LOC_GOVERNOR_PROMOTION_AQUACULTURE_DESCRIPTION",
+			"es_ES",
+			"Puede construir la mejora de la Pesquería. +1 [ICON_Housing] Vivienda. Los barcos de pesca adyacentes a una Pesquería producen +2 [ICON_Food] Alimentos; y otro +1 [ICON_Production] Producción mientras Liang esté establecido en esta ciudad."
+		),
+		(
+			"LOC_IMPROVEMENT_FISHERY_DESCRIPTION",
+			"es_ES",
+			"Una mejora única de [ICON_Governor] Gobernador que se puede construir en una ciudad con un Gobernador topógrafo con la promoción de Acuicultura.[NEWLINE][NEWLINE]Esta ficha no se puede trabajar. Los barcos de pesca adyacentes a una o más pesquerías producen +2 [ICON_Food] Alimentos; y otro +1 [ICON_Production] Producción mientras Liang esté establecido en esta ciudad. +1 [ICON_Housing] Vivienda. Debe colocarse en una ficha de Costa o Lago adyacente a un recurso marítimo."
+		);
+--========================================================================================================================
+--========================================================================================================================
