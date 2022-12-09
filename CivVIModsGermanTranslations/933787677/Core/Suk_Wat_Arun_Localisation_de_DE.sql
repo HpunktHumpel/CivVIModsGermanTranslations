43e8daa3ed3c94edacf081b99512befcf000487c
H.Humpel <H.Humpel@gmx.de>
2022-12-09 19:30:25 +0100

    [IMPROVED] Sukritact's Wat Arun

--- CivVIModsGermanTranslations/933787677/Core/Suk_Wat_Arun_Localisation_de_DE.sql
+++ CivVIModsGermanTranslations/933787677/Core/Suk_Wat_Arun_Localisation_de_DE.sql
@@ -2,18 +2,21 @@
 -- LocalizedText
 --========================================================================================================================
 INSERT OR REPLACE INTO LocalizedText
-		(Tag,					Language,	Text)
+		(Tag, Language, Text, gender, plurality)
+VALUES
+		("LOC_BUILDING_SUK_WAT_ARUN_NAME", "de_DE", "Wat Arun|Wat Arun|Wat Arun|Wat Arun|Wat Arun", "masculine:no_article", "1");
+
+INSERT OR REPLACE INTO LocalizedText
+		(Tag, Language, Text)
 VALUES
 --========================================================================================================================
 -- SIAM
 --========================================================================================================================
-		("LOC_BUILDING_SUK_WAT_ARUN_NAME",	'de_DE',
-		"Wat Arun"	),
-		("LOC_BUILDING_SUK_WAT_ARUN_DESCRIPTION",	'de_DE',
+		("LOC_BUILDING_SUK_WAT_ARUN_DESCRIPTION",	"de_DE",
 		"+2 [ICON_Housing] Wohnraum in all Euren Städten, die an einem Fluss liegen. +2 [ICON_Food] Nahrung in Bezirken, die an Wat Arun angrenzen.[NEWLINE]Muss an einem Fluss gebaut werden."	),
-		("LOC_PEDIA_WONDERS_PAGE_BUILDING_SUK_WAT_ARUN_CHAPTER_HISTORY_PARA_1",		'de_DE',
-		"Wat Arun, oder der Tempel der Morgenröte, ist ein buddhistischer Tempel am Ufer des Chao Phraya in Bangkok. Seit der Zeit des Ayutthaya-Königreichs existierte an der Stelle von Wat Arun ein Tempel. Der Tempel war damals unter dem Namen Wat Makok bekannt, benannt nach dem Dorf Bang Makok, in dem er lag. Laut dem Historiker Prinz Damrong Rajanubhab wurde der Tempel während der Regierungszeit von König Narai (1656–1688) auf französischen Karten verzeichnet. Somit ist zumindest bekannt, dass er zu diesem Zeitpunkt existiert hat, obwohl das genaue Datum der Gründung des Tempels nicht bekannt ist. Der Tempel wurde von König Taksin in Wat Chaeng umbenannt, als er nach dem Fall von Ayutthaya in der Nähe des Tempels seine neue Hauptstadt Thonburi gründete. Es wird gesagt, dass Taksin geschworen hat, den Tempel zu restaurieren, nachdem er ihn im Morgengrauen passiert hat. Der Tempel beherbergte die Smaragd-Buddha-Statue, bevor diese 1785 in den Wat Phra Kaeo am östlichen Ufer des Flusses verlegt wurde. Der Tempel befand sich während der Herrschaft von Taksin auf dem Gelände des königlichen Palastes, bevor sein Nachfolger Rama I. den Palast auf die andere Seite des Flusses verlegte. Daraufhin blieb der Tempel für eine Zeit lang verlassen, bis Rama II. ihn restaurieren lies und dabei auch die Pagode auf 70 Meter verlängerte. Der Blick auf den Tempel bei Nacht oder bei Sonnenuntergang ist vielleicht einer der ikonischsten Ausblicke auf Bangkok."),				
-		("LOC_BUILDING_SUK_WAT_ARUN_QUOTE",	'de_DE',
+		("LOC_PEDIA_WONDERS_PAGE_BUILDING_SUK_WAT_ARUN_CHAPTER_HISTORY_PARA_1",		"de_DE",
+		"Wat Arun, oder der Tempel der Morgenröte, ist ein buddhistischer Tempel am Ufer des Chao Phraya in Bangkok. Seit der Zeit des Ayutthaya-Königreichs existierte an der Stelle von Wat Arun ein Tempel. Der Tempel war damals unter dem Namen Wat Makok bekannt, benannt nach dem Dorf Bang Makok, in dem er lag. Laut dem Historiker Prinz Damrong Rajanubhab wurde der Tempel während der Regierungszeit von König Narai (1656–1688) auf französischen Karten verzeichnet. Somit ist zumindest bekannt, dass er zu diesem Zeitpunkt existiert hat, obwohl das genaue Datum der Gründung des Tempels nicht bekannt ist. Der Tempel wurde von König Taksin in Wat Chaeng umbenannt, als er nach dem Fall von Ayutthaya in der Nähe des Tempels seine neue Hauptstadt Thonburi gründete. Es wird gesagt, dass Taksin geschworen hat, den Tempel zu restaurieren, nachdem er ihn im Morgengrauen passiert hat. Der Tempel beherbergte die Smaragd-Buddha-Statue, bevor diese 1785 in den Wat Phra Kaeo am östlichen Ufer des Flusses verlegt wurde. Der Tempel befand sich während der Herrschaft von Taksin auf dem Gelände des königlichen Palastes, bevor sein Nachfolger Rama I. den Palast auf die andere Seite des Flusses verlegte. Daraufhin blieb der Tempel für eine Zeit lang verlassen, bis Rama II. ihn restaurieren lies und dabei auch die Pagode auf 70 Meter verlängerte. Der Blick auf den Tempel bei Nacht oder bei Sonnenuntergang ist vielleicht einer der ikonischsten Ausblicke auf Bangkok."),
+		("LOC_BUILDING_SUK_WAT_ARUN_QUOTE",	"de_DE",
 		"“Um Mitternacht kamen wir auf einem Boot an, als Mondlicht die Wolken in Gold tauchte. Wat Arun, brillant, wie es strahlte. Bei diesem Anblick hielt ich die Tränen zurück.”[NEWLINE]– Sunthorn Phu"	);
 --------------------------------------------------------------------
 --------------------------------------------------------------------
