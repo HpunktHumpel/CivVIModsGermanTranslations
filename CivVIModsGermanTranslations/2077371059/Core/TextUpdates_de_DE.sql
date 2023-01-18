0e88e71c7f0755b9b1c4cd7ca72df45eb25d15c1
H.Humpel <H.Humpel@gmx.de>
2023-01-18 20:12:47 +0100

    [FIXED] Alchemist Unit
    LoadOrder fixed

--- CivVIModsGermanTranslations/2077371059/Core/TextUpdates_de_DE.sql
+++ CivVIModsGermanTranslations/2077371059/Core/TextUpdates_de_DE.sql
@@ -1,3 +1,3 @@
-UPDATE LocalizedText SET Text = "Baut 3 unterschiedliche Spezialbezirke oder 3 Alchemisten-Ateliers." WHERE Tag = "LOC_BOOST_TRIGGER_MATHEMATICS";
+UPDATE LocalizedText SET Text = "Baut 3 unterschiedliche Spezialbezirke oder 3 Alchemisten-Ateliers." WHERE Tag = "LOC_BOOST_TRIGGER_MATHEMATICS" AND Language = "de_DE";
 INSERT OR REPLACE INTO LocalizedText (Language, Tag, Text)
 VALUES ("de_DE", "LOC_BOOST_TRIGGER_SAILOR_MATHEMATICS_PLACEHOLDER", ""), ("de_DE", "LOC_BOOST_TRIGGER_LONGDESC_SAILOR_MATHEMATICS_PLACEHOLDER", "");
