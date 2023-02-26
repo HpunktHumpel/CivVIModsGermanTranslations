17934c666ac04b0c90d78681ef9a54f355870529
H.Humpel <H.Humpel@gmx.de>
2023-02-26 19:28:04 +0100

    [UPDATE] Leugi's Preserve Rework

--- CivVIModsGermanTranslations/2879431839/Text/PreserveAdjustments_Text_de_DE.sql
+++ CivVIModsGermanTranslations/2879431839/Text/PreserveAdjustments_Text_de_DE.sql
@@ -6,15 +6,15 @@ INSERT OR REPLACE INTO LocalizedText
 VALUES
 		("de_DE", 
 		"LOC_DISTRICT_PRESERVE_DESCRIPTION",
-		"Ein Bezirk der Vorteile aus der Anziehungskraft umgebender Felder zieht. Verursacht Kulturschock für angrenzendes herrenloses Gebiet und erhöht ihre Anziehungskraft um 1.[NEWLINE][NEWLINE]Gewährt bis zu 3 [ICON_SCIENCE] Wissenschaft basierend auf Anziehungskraft.[NEWLINE][NEWLINE]Kann auf Wald, Regenwald und Schwemmland gebaut werden. Beim Bau auf atemberaubenden Feldern, können andere Bezirke auf der selben Geländeart gebaut werden.[NEWLINE][NEWLINE]+1 [ICON_SCIENCE] Wissenschaft für angrenzende [ICON_APPEAL] bezaubernde Landfelder. [NEWLINE]+2 [ICON_SCIENCE] Wissenschaft für angrenzendes, nicht-modernisiertes, [ICON_APPEAL] atemberaubendes Land.[NEWLINE]+1 [ICON_SCIENCE] Wissenschaft für angrenzende Küstenfelder.[NEWLINE]+2 [ICON_SCIENCE] Wissenschaft für angrenzende, nicht-modernisierte Küstenfelder mit Geländerarten."),		
+		"Das Schutzgebiet zieht seine Vorteile aus der Anziehungskraft umgebender Felder. Verursacht Kulturschock für angrenzendes herrenloses Gebiet und erhöht ihre Anziehungskraft um 1.[NEWLINE][NEWLINE]Gewährt bis zu 3 [ICON_SCIENCE] Wissenschaft basierend auf Anziehungskraft.[NEWLINE][NEWLINE]Kann auf Wald, Regenwald, Sumpf und Schwemmland gebaut werden. Beim Bau auf atemberaubenden Feldern, können andere Bezirke auf der selben Geländeart gebaut werden."),
 
 		("de_DE", 
 		"LOC_BUILDING_GROVE_DESCRIPTION",
-		"Gewährt [ICON_FAITH] Glauben auf Basis des [ICON_SCIENCE] Wissenschaftsbonus des Schutzgebiets.[NEWLINE][NEWLINE]+1 [ICON_FAITH] Glauben für angrenzende [ICON_APPEAL] bezaubernde Landfelder.[NEWLINE]+2 [ICON_FAITH] Glauben für angrenzendes, nicht-modernisiertes, [ICON_APPEAL] atemberaubendes Land.[NEWLINE]+1 [ICON_FAITH] Glauben für angrenzende Küstenfelder.[NEWLINE]+2 [ICON_FAITH] Glauben für angrenzende, nicht-modernisierte Küstenfelder mit Geländerarten."),
+		"Gewährt [ICON_FAITH] Glauben auf Basis des [ICON_SCIENCE] Wissenschaftsbonus des Schutzgebiets.[NEWLINE][NEWLINE]+1 [ICON_FAITH] Glauben und [ICON_SCIENCE] Wissenschaft für angrenzende bezaubernde Landfelder. +2 [ICON_FAITH] Glauben und [ICON_SCIENCE] Wissenschaft für angrenzendes, nicht-modernisiertes, atemberaubendes Land.[NEWLINE]+1 [ICON_FAITH] Glauben für angrenzende Küstenfelder. +2 [ICON_FAITH] Glauben und [ICON_SCIENCE] Wissenschaft für angrenzende, nicht-modernisierte Küstenfelder mit Geländerarten."),
 
 		("de_DE", 
 		"LOC_BUILDING_SANCTUARY_DESCRIPTION",
-		"Verdoppelt den [ICON_FAITH] Glaubens- und [ICON_SCIENCE] Wissenschaftsbonus der an angrenzende Felder des Schutzgebiets und seiner Gebäude gewährt wird.[NEWLINE]Ermöglicht den Kauf von Naturforschern mit [ICON_FAITH] Glauben.[NEWLINE][NEWLINE]+1 [ICON_FAVOR] diplomatische Gunst beim Bau auf atemberaubendem Feld."),
+		"Verdoppelt den [ICON_FAITH] Glaubens- und [ICON_SCIENCE] Wissenschaftsbonus der angrenzenden Feldern des Hains.[NEWLINE]Ermöglicht den Kauf von Naturforschern mit [ICON_FAITH] Glauben.[NEWLINE][NEWLINE]+1 [ICON_FAVOR] diplomatische Gunst beim Bau auf atemberaubendem Feld."),
 		
 		("de_DE", 
 		"LOC_DISTRICT_PRESERVE_APPEAL_SCIENCE_BREATHTAKING",					
@@ -32,6 +32,10 @@ VALUES
 		"LOC_DISTRICT_PRESERVE_FAITH",					
 		"+{1_num} [ICON_FAITH] Glauben durch angrenzendes Schutzgebiet.");
 
-UPDATE LocalizedText
-SET Text = REPLACE(Text,"Requires a Sanctuary to be purchased.", " Für den Kauf wird eine Zuflucht benötigt.")
-WHERE Language = "de_DE" AND Tag = "LOC_UNIT_NATURALIST_DESCRIPTION";
+INSERT OR REPLACE INTO LocalizedText
+		(Language, Tag,	Text)
+VALUES
+		("de_DE", 
+		"LOC_NATURALIST_REQUIREMENT",					
+		"Für den Kauf wird eine Zuflucht benötigt.");
+
