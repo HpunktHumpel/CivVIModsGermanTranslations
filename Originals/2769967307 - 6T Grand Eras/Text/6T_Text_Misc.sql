68365fcbb7581ea2031a833e44d70a8a03c89b8b
H.Humpel <H.Humpel@gmx.de>
2023-02-26 20:44:54 +0100

    [UPDATE] Grand Eras (Project 6T Core)

---
+++ Originals/2769967307 - 6T Grand Eras/Text/6T_Text_Misc.sql
@@ -0,0 +1,34 @@
+-- 6T_Text_Misc
+-- Author: JNR
+--------------------------------------------------------------
+
+-- English
+--------------------------------------------------------------
+INSERT OR REPLACE INTO EnglishText
+		(Tag,															Text)
+VALUES	('LOC_GREATPERSON_COMBAT_STRENGTH_AOE_CLASSICAL_6T_LAND',		'+5 [ICON_Strength] Combat Strength and +1 [ICON_Movement] Movement to Classical and Post-Classical era land units within 2 tiles.'),
+		('LOC_GREATPERSON_COMBAT_STRENGTH_AOE_CLASSICAL_6T_SEA',		'+5 [ICON_Strength] Combat Strength and +1 [ICON_Movement] Movement to Classical and Post-Classical era naval units within 2 tiles.'),
+		('LOC_GREATPERSON_COMBAT_STRENGTH_AOE_6T_POST_CLASSICAL_LAND',	'+5 [ICON_Strength] Combat Strength and +1 [ICON_Movement] Movement to Post-Classical and Medieval era land units within 2 tiles.'),
+		('LOC_GREATPERSON_COMBAT_STRENGTH_AOE_6T_POST_CLASSICAL_SEA',	'+5 [ICON_Strength] Combat Strength and +1 [ICON_Movement] Movement to Post-Classical and Medieval era naval units within 2 tiles.'),
+		
+		('LOC_POLICY_AGOGE_DESCRIPTION_6T',								'+50% [ICON_Production] Production toward Ancient and Classical era melee, anti-cavalry, ranged, and recon units.'),
+		('LOC_POLICY_FEUDAL_CONTRACT_DESCRIPTION_6T',					'+50% [ICON_Production] Production toward Medieval era and earlier melee, anti-cavalry, ranged, and recon units.'),
+		('LOC_POLICY_GRANDE_ARMEE_DESCRIPTION_6T',						'+50% [ICON_Production] Production toward Industrial era and earlier melee, anti-cavalry, ranged, and recon units.'),
+		('LOC_POLICY_MILITARY_FIRST_DESCRIPTION_6T',					'+50% [ICON_Production] Production toward all melee, anti-cavalry, ranged, and recon units.'),
+		('LOC_POLICY_CHIVALRY_DESCRIPTION_6T',							'+50% [ICON_Production] Production toward Medieval era and earlier heavy and light cavalry units.'),
+		('LOC_POLICY_PRESS_GANGS_DESCRIPTION_6T',						'+100% [ICON_Production] Production toward Industrial era and earlier naval units.'),
+		
+		('LOC_POLICY_CORVEE_DESCRIPTION_6T',							'+15% [ICON_Production] Production toward Post-Classical era and earlier wonders.'),
+		('LOC_POLICY_GOTHIC_ARCHITECTURE_DESCRIPTION_6T',				'+15% [ICON_Production] Production toward Industrial era and earlier wonders.'),
+		
+		('LOC_POLICY_6T_DRESSAGE_NAME',									'Dressage'),
+		('LOC_POLICY_6T_DRESSAGE_DESCRIPTION',							'+50% [ICON_Production] Production toward Industrial era and earlier heavy and light cavalry units.'),
+		('LOC_POLICY_6T_CLINKER_NAME',									'Clinker Hulls'),
+		('LOC_POLICY_6T_CLINKER_DESCRIPTION',							'+100% [ICON_Production] Production toward Medieval era and earlier naval units.'),
+
+		('LOC_GREATWORK_ARTIFACT_6T_01_NAME',							'Post-Classical Artifact A'),
+		('LOC_GREATWORK_ARTIFACT_6T_02_NAME',							'Post-Classical Artifact B'),
+		('LOC_GREATWORK_ARTIFACT_6T_03_NAME',							'Post-Classical Artifact C'),
+		('LOC_GREATWORK_ARTIFACT_6T_04_NAME',							'Post-Classical Artifact D'),
+		('LOC_GREATWORK_ARTIFACT_6T_05_NAME',							'Post-Classical Artifact E');
+--------------------------------------------------------------
---
+++ Originals/2769967307 - 6T Grand Eras/Text/6T_Text_Names.sql
@@ -0,0 +1,63 @@
+-- 6T_Text_Names
+-- Author: JNR
+--------------------------------------------------------------
+
+-- English
+--------------------------------------------------------------
+INSERT OR REPLACE INTO EnglishText
+		(Tag,									Text)
+VALUES	('LOC_TECH_6T_FISHING_NAME',			'Fishing'),
+		('LOC_TECH_6T_CALENDAR_NAME',			'Calendar'),
+		('LOC_TECH_6T_SURVEYING_NAME',			'Surveying'),
+		('LOC_TECH_6T_SADDLE_NAME',				'Saddle'),
+		('LOC_TECH_6T_OPTICS_NAME',				'Optics'),
+		('LOC_TECH_6T_METALLURGY_NAME',			'Metallurgy'),
+		('LOC_TECH_6T_SIEGE_ENGINES_NAME',		'Siege Engines'),
+		('LOC_TECH_6T_HORSE_COLLAR_NAME',		'Horse Collar'),
+		('LOC_TECH_6T_PAPER_NAME',				'Paper'),
+		('LOC_TECH_6T_MECHANICS_NAME',			'Mechanics'),
+		('LOC_TECH_6T_COMPASS_NAME',			'Compass'),
+		('LOC_TECH_6T_ALCHEMY_NAME',			'Alchemy'),
+		('LOC_TECH_6T_PIKE_TACTICS_NAME',		'Pike Tactics'),
+		('LOC_TECH_6T_SPINNING_WHEEL_NAME',		'Spinning Wheel'),
+		('LOC_TECH_6T_CARACOLE_NAME',			'Caracole'),
+		('LOC_TECH_6T_LINE_TACTICS_NAME',		'Line Tactics'),
+		('LOC_TECH_6T_BIOLOGY_NAME',			'Biology'),
+		('LOC_TECH_6T_COKING_NAME',				'Coking'),
+		('LOC_TECH_6T_MEDICINE_NAME',			'Medicine'),
+		('LOC_TECH_6T_DYNAMITE_NAME',			'Dynamite'),
+		('LOC_TECH_6T_WARPLANES_NAME',			'Warplanes'),
+		('LOC_TECH_6T_PHARMACEUTICALS_NAME',	'Pharmaceuticals'),
+		('LOC_TECH_6T_REFRIGERATION_NAME',		'Refrigeration'),
+		('LOC_TECH_6T_PHYSICS_NAME',			'Physics'),
+		('LOC_TECH_6T_ECOLOGY_NAME',			'Ecology'),
+		('LOC_TECH_6T_GENOMICS_NAME',			'GENOMICS'),
+
+		('LOC_TECH_6T_REQUISITIONS_NAME',		'Requisitions'),
+		('LOC_TECH_6T_FERTILIZERS_NAME',		'Fertilizers'),
+		('LOC_TECH_6T_RADAR_NAME',				'Radar'),
+		('LOC_TECH_6T_3D_PRINTING_NAME',		'3D Printing'),
+		('LOC_TECH_6T_BLOCKCHAIN_NAME',			'Blockchain'),
+		('LOC_TECH_6T_HABITATS_NAME',			'Habitats'),
+
+		('LOC_CIVIC_6T_WARFARE_NAME',			'Warfare'),
+		('LOC_CIVIC_6T_TERRITORY_NAME',			'Territory'),
+		('LOC_CIVIC_6T_CONSECRATION_NAME',		'Consecration'),
+		('LOC_CIVIC_6T_CITIZENSHIP_NAME',		'Citizenship'),
+		('LOC_CIVIC_6T_STATE_RELIGION_NAME',	'State Religion'),
+		('LOC_CIVIC_6T_MONASTICISM_NAME',		'Monasticism'),
+		('LOC_CIVIC_6T_MERCHANT_LEAGUES_NAME',	'Merchant Leagues'),
+		('LOC_CIVIC_6T_ESTATES_NAME',			'Estates'),
+		('LOC_CIVIC_6T_PATRONAGE_NAME',			'Patronage'),
+		('LOC_CIVIC_6T_TOLERANCE_NAME',			'Tolerance'),
+		('LOC_CIVIC_6T_PARLIAMENTARISM_NAME',	'Parliamentarism'),
+		('LOC_CIVIC_6T_ROMANTICISM_NAME',		'Romanticism'),
+		('LOC_CIVIC_6T_CONSUMERISM_NAME',		'Consumerism'),
+		('LOC_CIVIC_6T_POP_CULTURE_NAME',		'Pop Culture'),
+
+		('LOC_CIVIC_6T_EMPIRICISM_NAME',		'Empiricism'),
+		('LOC_CIVIC_6T_SOVEREIGNTY_NAME',		'Sovereignty'),
+		('LOC_CIVIC_6T_CONSTITUTION_NAME',		'Constitution'),
+		('LOC_CIVIC_6T_GEOPOLITICS_NAME',		'Geopolitics'),
+		('LOC_CIVIC_6T_EGOVERNANCE_NAME',		'E-Governance');
+--------------------------------------------------------------
