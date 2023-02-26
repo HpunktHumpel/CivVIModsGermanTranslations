609eae2df11b7d42706cd7823ac0719c2367aa1c
H.Humpel <H.Humpel@gmx.de>
2023-02-26 19:59:07 +0100

    [UPDATE] Sukritact's Simple UI Adjustments

--- Originals/939149009 - Sukritact s Simple UI Adjustments/Core/Localisation.sql
+++ Originals/939149009 - Sukritact s Simple UI Adjustments/Core/Localisation.sql
@@ -15,215 +15,215 @@ VALUES
 	-----------------------------------------------------
 		(
 			"LOC_SUK_SCREENSHOT_MODE_NAME",
-			"de_DE",
-			"Bildschirmfoto-Modus"
+			"en_US",
+			"Screenshot Mode"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_TT",
-			"de_DE",
-			"Linksklicke zum Aktivieren des Bildschirmfoto-Modus mit den aktuellen Einstellungen. Rechtsklicke um das Einstellungsmenü des Bildschirmfoto-Modus zu öffnen."
+			"en_US",
+			"Left-click to enter Screenshot Mode with the current settings. Right-click to open the Screenshot Mode options panel."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_DESC",
-			"de_DE",
-			"Bildschirmfoto-Modus aufrufen.[NEWLINE][NEWLINE]Im Bildschirmfoto-Modus werden die meisten oder alle Elemente der Benutzeroberfläche ausgeblendet, so dass ungestörte Bildschirmfotos gemacht werden können.[NEWLINE][NEWLINE]Der Bildschirm kann mit den WASD-Tasten geschwenkt, mit den Q- und E-Tasten gedreht und, wenn ''Feste Neigung'' aktiviert ist, mit den Z- und C-Tasten gekippt werden. Die Kameradrehung wird in diesem Modus nicht zurückgesetzt.[NEWLINE][NEWLINE]Drücken Sie ESC, um den Bildschirmfoto-Modus zu verlassen."
+			"en_US",
+			"Enter Screenshot Mode.[NEWLINE][NEWLINE] Screenshot Mode will hide most or all UI elements, allowing you to take unobstructed screenshots.[NEWLINE][NEWLINE] You may pan the screen with the WASD keys; rotate with the Q and E keys; hold Alt and use the Mouse Wheel to adjust Time of Day; and if Fixed Tilt is enabled, you can adjust the tilt with the Z and C keys. Camera rotation will not snap back while in this mode.[NEWLINE][NEWLINE] Press ESC to exit Screenshot Mode."
 		),
 	-----------------------------------------------------
 	-- Hotkeys
 	-----------------------------------------------------
 		(
 			"LOC_OPTIONS_HOTKEY_SUK_SCREENSHOT_MODE",
-			"de_DE",
-			"{LOC_SUK_SCREENSHOT_MODE_NAME} aufrufen"
+			"en_US",
+			"Enter {LOC_SUK_SCREENSHOT_MODE_NAME}"
 		),
 		(
 			"LOC_OPTIONS_HOTKEY_SUK_SCREENSHOT_MODE_HELP",
-			"de_DE",
-			"Ruft {LOC_SUK_SCREENSHOT_MODE_NAME} auf"
+			"en_US",
+			"Enters {LOC_SUK_SCREENSHOT_MODE_NAME}"
 		),
 	-----------------------------------------------------
 	-- Options
 	-----------------------------------------------------
 		(
 			"LOC_SUK_SCREENSHOT_MODE_FIXED_TILT_NAME",
-			"de_DE",
-			"Feste Neigung verwenden"
+			"en_US",
+			"Use Fixed Tilt"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_FIXED_TILT_DESC",
-			"de_DE",
-			"Feste Neigung im Bildschirmfoto-Modus verwenden. Sie können die Neigung mit Alt + Ziehen anpassen oder die Tasten Z und C nutzen.[NEWLINE][NEWLINE][COLOR_RED]Warnung: Bestimmte Winkel und Neigungen können das Spiel zum Absturz bringen. Seien Sie vorsichtig![ENDCOLOR]"
+			"en_US",
+			"Use Fixed Tilt in Screenshot Mode. You may Alt + Drag to adjust tilt, or use the Z and C keys.[NEWLINE][NEWLINE][COLOR_RED]Warning: Certain angles and tilts may cause the game to crash. Be cautious![ENDCOLOR]"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_UNIT_FLAG_NAME",
-			"de_DE",
-			"Einheitennadeln ausblenden"
+			"en_US",
+			"Hide Unit Flags"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_UNIT_FLAG_DESC",
-			"de_DE",
-			"Einheitennadeln im Bildschirmfoto-Modus ausblenden."
+			"en_US",
+			"Hide Unit Flags in Screenshot Mode."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_CITY_BANNER_NAME",
-			"de_DE",
-			"Stadtbanner ausblenden"
+			"en_US",
+			"Hide City Banners"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_CITY_BANNER_DESC",
-			"de_DE",
-			"Stadtbanner im Bildschirmfoto-Modus ausblenden."
+			"en_US",
+			"Hide City Banners in Screenshot Mode."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_BORDERS_NAME",
-			"de_DE",
-			"Staatsgrenzen ausblenden"
+			"en_US",
+			"Hide Borders"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_BORDERS_DESC",
-			"de_DE",
-			"Staatsgrenzen im Bildschirmfoto-Modus ausblenden."
+			"en_US",
+			"Hide Borders in Screenshot Mode."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_MAP_LABELS_NAME",
-			"de_DE",
-			"Kartenbeschreibungen ausblenden"
+			"en_US",
+			"Hide Map Labels"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_MAP_LABELS_DESC",
-			"de_DE",
-			"Kartenbeschreibungen im Bildschirmfoto-Modus ausblenden, falls sie nicht bereits ausgeblendet sind."
+			"en_US",
+			"Hide Map Labels in Screenshot Mode, if they aren't already hidden."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_YIELDS_NAME",
-			"de_DE",
-			"Felderträge ausblenden"
+			"en_US",
+			"Hide Yields"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_YIELDS_DESC",
-			"de_DE",
-			"Felderträge im Bildschirmfoto-Modus ausblenden, falls sie nicht bereits ausgeblendet sind."
+			"en_US",
+			"Hide Yields in Screenshot Mode, if they aren't already hidden."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_RESOURCE_ICONS_NAME",
-			"de_DE",
-			"Ressourcensymbole ausblenden"
+			"en_US",
+			"Hide Resource Icons"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_RESOURCE_ICONS_DESC",
-			"de_DE",
-			"Blendet Ressourcensymbole im Bildschirmfoto-Modus aus, wenn nicht bereits ausgeblendet."
+			"en_US",
+			"Hide Resource Icons in Screenshot Mode, if they aren't already hidden."
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_MAP_PINS_NAME",
-			"de_DE",
-			"Kartennadeln ausblenden"
+			"en_US",
+			"Hide Map Pins"
 		),
 		(
 			"LOC_SUK_SCREENSHOT_MODE_MAP_PINS_DESC",
-			"de_DE",
-			"Blendet Kartennadeln im Bildschirmfoto-Modus aus."
+			"en_US",
+			"Hide Map Pins in Screenshot Mode."
 		),
 --====================================================================================================================
 -- Production Tooltip
 --====================================================================================================================
 		(
 			"LOC_CITY_YIELD_FROM_BUILDINGS_SUMMARY_TOOLTIP",
-			"de_DE",
-			"{Value : number +#.#;-#.#} durch Gebäude (Summe)"
+			"en_US",
+			"{Value : number +#.#;-#.#} from Buildings (Total)"
 		),
 		(
 			"LOC_CITY_YIELD_FROM_DISTRICTS_SUMMARY_TOOLTIP",
-			"de_DE",
-			"{Value : number +#.#;-#.#} durch Bezirke (Summe)"
+			"en_US",
+			"{Value : number +#.#;-#.#} from Districts (Total)"
 		),
 
 		(
 			"LOC_CITY_GET_UNIT_PRODUCTION_MODIFIER",
-			"de_DE",
-			"{Value : number +#;-#}% ({ActualValue : number +#.#;-#.#}) für Einheiten"
+			"en_US",
+			"{Value : number +#;-#}% ({ActualValue : number +#.#;-#.#}) towards Units"
 		),
 		(
 			"LOC_CITY_GET_BUILDING_PRODUCTION_MODIFIER",
-			"de_DE",
-			"{Value : number +#;-#}% ({ActualValue : number +#.#;-#.#}) für Gebäude und Wunder"
+			"en_US",
+			"{Value : number +#;-#}% ({ActualValue : number +#.#;-#.#}) towards Buildings and Wonders"
 		),
 		(
 			"LOC_CITY_GET_BUILDING_NON_WONDER_PRODUCTION_MODIFIER",
-			"de_DE",
-			"{Value : number +#;-#}% ({ActualValue : number +#.#;-#.#}) für Gebäude"
+			"en_US",
+			"{Value : number +#;-#}% ({ActualValue : number +#.#;-#.#}) towards Buildings"
 		),
 --====================================================================================================================
 -- Growth and District Tooltips
 --====================================================================================================================
 		(
 			"LOC_SUK_DISTRICT_POPULATION_TOOLTIP",
-			"de_DE",
-			"Die Bezirksobergrenze wird gesteigert bei {2} [ICON_Citizen] Bevölkerung. Ihr könnt aktuell {1} Bezirke errichten."
+			"en_US",
+			"The District cap will next increase at {2} [ICON_Citizen] Population. You can currently build up to {1} Districts."
 		),
 		(
 			"LOC_SUK_CITY_YIELD_FROM_POPULATION_TOOLTIP",
-			"de_DE",
-			"{1} durch Bevölkerung"
+			"en_US",
+			"{1} from Population"
 		),
 		(
 			"LOC_SUK_CITY_YIELD_FROM_HAPPINESS_TOOLTIP",
-			"de_DE",
-			"{1}% ({2}) durch Annehmlichkeiten"
+			"en_US",
+			"{1}% ({2}) from Amenities"
 		),
 		(
 			"LOC_SUK_CITY_YIELD_FROM_HOUSING_TOOLTIP",
-			"de_DE",
-			"{1}% ({2}) durch Wohnraum"
+			"en_US",
+			"{1}% ({2}) from Housing"
 		),
 		(
 			"LOC_SUK_CITY_YIELD_FROM_OCCUPATION_TOOLTIP",
-			"de_DE",
-			"{1}% ({2}) durch Besatzung"
+			"en_US",
+			"{1}% ({2}) from Occupation"
 		),
 --====================================================================================================================
 -- Loyalty Tooltips
 --====================================================================================================================
 		(
 			"LOC_SUK_LOYALTY_HEADER_TOOLTIP",
-			"de_DE",
-			"Druck durch umgebende Stadtbevölkerung"
+			"en_US",
+			"Pressure from nearby Citizens"
 		),
 		(
 			"LOC_SUK_LOYALTY_TOTAL_POPULATION_PRESSURE_TOOLTIP",
-			"de_DE",
-			"Summe Bürger Druck"
+			"en_US",
+			"Total Pop. Pressure"
 		),
 		(
 			"LOC_SUK_LOYALTY_CAP_INFO_TOOLTIP",
-			"de_DE",
-			"Bevölkerungsdruck skaliert basierend aufgrund der Rate zwischen inländischen und ausländischen Drücken. Bevölkerungsdruck wird begrenzt auf ±20."
+			"en_US",
+			"Population Pressure scales based on the ratio between Domestic and Foreign Pressure. Population Pressure is always capped at ±20."
 		),
 		(
 			"LOC_SUK_LOYALTY_CAP_TOOLTIP",
-			"de_DE",
-			"(begrenzt auf ±20)"
+			"en_US",
+			"(will be capped at ±20)"
 		),
 --====================================================================================================================
 -- City Panel Overview
 --====================================================================================================================
 		(
 			"LOC_HUD_REPORTS_SUK_OTHER_ADJACENCY_BONUS",
-			"de_DE",
-			"Sonstige Bonusse"
+			"en_US",
+			"Other Bonuses"
 		),
 --====================================================================================================================
 -- Misc.
 --====================================================================================================================
 		(
 			"LOC_CITY_YIELD_FROM_GREATWORKS_TOOLTIP",
-			"de_DE",
-			"{Value : number +#.#;-#.#} durch Große Werke"
+			"en_US",
+			"{Value : number +#.#;-#.#} from Great Works"
 		),
 		(
 			"LOC_OPTIONS_HOTKEY_CATEGORY_SUK_HOTKEYS",
-			"de_DE",
-			"Benuteroberfläche (Sukritact’s Mods):"
+			"en_US",
+			"User Interface (Sukritact’s Mods):"
 		);
 --====================================================================================================================
 --====================================================================================================================
