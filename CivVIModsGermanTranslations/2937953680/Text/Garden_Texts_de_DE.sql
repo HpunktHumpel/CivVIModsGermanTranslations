4b6eae1894609fed55bbb064cbd5dcbdd204b422
H.Humpel <H.Humpel@gmx.de>
2023-06-03 01:52:03 +0200

    [UPDATE] Leugi's Greenery: Garden District

--- CivVIModsGermanTranslations/2937953680/Text/Garden_Texts_de_DE.sql
+++ CivVIModsGermanTranslations/2937953680/Text/Garden_Texts_de_DE.sql
@@ -4,20 +4,20 @@
 INSERT OR REPLACE INTO LocalizedText
 		(Language, Tag,	Text, Gender, Plurality)
 VALUES
-		("de_DE",
-		"LOC_DISTRICT_LEU_GARDEN_NAME",
+		("de_DE", 
+		"LOC_DISTRICT_LEU_GARDEN_NAME",					
 		"Garten|Garten|Garten|Gartens|Garten",
-		"masculine", 1),
-
-		("de_DE",
-		"LOC_BUILDING_LEU_PAVILLION_NAME",
+		'masculine',1),
+		
+		("de_DE", 
+		"LOC_BUILDING_LEU_PAVILLION_NAME",					
 		"Pavillon|Pavillon|Pavillon|Pavillons|Pavillon",
-		"neuter", 1),
+		'masculine',1),
 
-		("de_DE",
-		"LOC_BUILDING_LEU_CONSERVATORY_NAME",
+		("de_DE", 
+		"LOC_BUILDING_LEU_CONSERVATORY_NAME",					
 		"Gewächshaus|Gewächshaus|Gewächshaus|Gewächshauses|Gewächshaus",
-		"neuter", 1);
+		'neuter',1);
 
 
 INSERT OR REPLACE INTO LocalizedText
@@ -25,72 +25,86 @@ INSERT OR REPLACE INTO LocalizedText
 VALUES
 --==========================================================================================================================
 -- Garden
---==========================================================================================================================
-
-
-		("de_DE",
-		"LOC_DISTRICT_LEU_GARDEN_DESC",
+--==========================================================================================================================		
+		
+		
+		("de_DE", 
+		"LOC_DISTRICT_LEU_GARDEN_DESC",					
 		"Der Garten nimmt seine Vorteile aus der Anziehungskraft umgebender Felder. Erhöht die Anziehungskraft umgebender Felder um 1.[NEWLINE][NEWLINE]Gewährt bis zu +3 [ICON_CULTURE] Kultur basierend auf der Anziehungskraft des Feldes.[NEWLINE][NEWLINE]Kann auf Wald, Regenwald, Sumpf und Schwemmland gebaut werden. Beim Bau auf einem Feld mit atemberaubender Anziehungskraft, erlaubt der Garten auch anderen Bezirken der Stadt den Bau auf dessen Geländearten."),
 
-		("de_DE",
-		"LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_LEU_GARDEN_CHAPTER_HISTORY_PARA_1",
+		("de_DE", 
+		"LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_LEU_GARDEN_CHAPTER_HISTORY_PARA_1",					
 		"Alle Gärten sind wichtig. Blühende Grünflächen schaffen Lebensräume für Wildtiere, tragen zur Kühlung unserer Städte bei, schützen vor Überschwemmungen und bringen die Menschen mit der Natur in Verbindung und schaffen wirtschaftlichen Wert durch Unternehmen, Arbeitsplätze und Tourismus. Der wichtigste Garten einer Stadt ist oft der Botanische Garten der Stadt.[NEWLINE][NEWLINE]Botanische Gärten sind Gärten mit einer dokumentierten Sammlung lebender Pflanzen zum Zweck der wissenschaftlichen Forschung, Erhaltung, Ausstellung und Bildung. In der Regel sind die Pflanzen mit ihrem botanischen Namen gekennzeichnet, aber auf den Schildern steht meist auch ihr gewöhnlicher Name. Sie können spezialisierte Pflanzensammlungen wie Kakteen und andere Sukkulenten, Kräutergärten, Pflanzen aus bestimmten Teilen der Welt sowie Gewächshäuser, Schattenhäuser, Pavillons und Wintergärten mit speziellen Sammlungen wie tropischen Pflanzen, alpinen Pflanzen oder anderen exotischen Pflanzen enthalten.[NEWLINE][NEWLINE]Botanische Gärten werden häufig von Universitäten oder anderen wissenschaftlichen Forschungseinrichtungen betrieben und verfügen oft über angeschlossene Herbarien und Forschungsprogramme zur Pflanzentaxonomie oder zu anderen Aspekten der botanischen Wissenschaft. Grundsätzlich besteht ihre Aufgabe darin, dokumentierte Sammlungen lebender Pflanzen zum Zwecke der wissenschaftlichen Forschung, der Erhaltung, der Ausstellung und der Bildung zu unterhalten, obwohl dies von den verfügbaren Ressourcen und den speziellen Interessen abhängt, die in jedem einzelnen Garten verfolgt werden.[NEWLINE]Königliche Gärten, die der wirtschaftlichen Nutzung oder der Ausstellung vorbehalten waren und zumindest einige Pflanzen enthielten, die durch spezielle Sammelreisen oder militärische Kampagnen im Ausland gewonnen wurden, sind aus dem zweiten Jahrtausend v. Chr. im alten Ägypten, Mesopotamien, Kreta, Mexiko und China bekannt. Um 2800 v. Chr. soll der chinesische Kaiser Shen Nung Sammler in weit entfernte Regionen geschickt haben, um nach Pflanzen mit wirtschaftlichem oder medizinischem Wert zu suchen.[NEWLINE][NEWLINE]In jüngster Zeit steht die Sensibilisierung für die Bedrohung der Ökosysteme der Erde durch die menschliche Bevölkerung und der daraus resultierende Bedarf an biologischen und physischen Ressourcen im Mittelpunkt. Botanische Gärten sind ein hervorragendes Medium für die Kommunikation zwischen der Welt der botanischen Wissenschaft und der breiten Öffentlichkeit. Bildungsprogramme können der Öffentlichkeit helfen, ein größeres Umweltbewusstsein zu entwickeln, indem sie die Bedeutung und Wichtigkeit von Ideen wie Erhaltung und Nachhaltigkeit verstehen."),
 
 --==========================================================================================================================
 -- Pavilion
---==========================================================================================================================
+--==========================================================================================================================		
 
 
-		("de_DE",
-		"LOC_BUILDING_LEU_PAVILLION_DESC",
+		("de_DE", 
+		"LOC_BUILDING_LEU_PAVILLION_DESC",					
 		"Gewährt [ICON_Gold] Gold in Höhe des [ICON_CULTURE] Kultertrags des Gartens.[NEWLINE][NEWLINE]+1 [ICON_Gold] Gold und [ICON_CULTURE] Kultur für angrenzende bezaubernde Landfelder.+2 [ICON_GOLD] Gold und [ICON_Culture] Kultur für angrenzende unverbesserte atemberaubende Landfelder.[NEWLINE]+1 [ICON_Gold] Gold und [ICON_Culture] Kultur für angrenzende Küstenfelder. +2 [ICON_Gold] Gold und [ICON_Culture] Kultur für angrenzende unverbesserte Küstenfelder mit Geländearten."),
 
-		("de_DE",
-		"LOC_PEDIA_BUILDINGS_PAGE_BUILDING_LEU_PAVILLION_CHAPTER_HISTORY_PARA_1",
+		("de_DE", 
+		"LOC_PEDIA_BUILDINGS_PAGE_BUILDING_LEU_PAVILLION_CHAPTER_HISTORY_PARA_1",					
 		"Pavillons können kleine Nebengebäude im Garten sein, ähnlich einem Gartenhaus oder einem Kiosk. Das Wort kann sich auch auf kleine, nur über das Dach erreichbare Räume auf dem Dach eines großen Hauses beziehen, die bis zum 18. Jahrhundert besonders beliebt waren und mit der italienischen Casina gleichgesetzt werden können. In der Regel freistehend, sind Pavillons häufig in botanischen und anderen Gärten zu finden.[NEWLINE][NEWLINE]Ursprünglich handelte es sich um ein temporäres oder halbpermanentes Bauwerk, das wie das moderne Zelt für besondere Anlässe wie Feste, Gartenbankette und Bälle errichtet wurde, aber es wurde immer dauerhafter, und im späten 17. [Ein Pavillon ist oft eine offene architektonische Struktur in städtischen Parks, die Unterschlupf oder eine Plattform für Menschen bietet, um sich zu versammeln oder aufzutreten, wie ein Musikpavillon oder ein Musikpavillon. Moderne Pavillons haben Architekten die Möglichkeit gegeben, mit verschiedenen Materialien zu experimentieren und neue strukturelle Formen zu erproben, wie z. B. der Serpentine Pavilion im Hyde Park, der sich jedes Jahr verändert."),
 
 --==========================================================================================================================
 -- Conservatory
---==========================================================================================================================
+--==========================================================================================================================		
 
-		("de_DE",
-		"LOC_BUILDING_LEU_CONSERVATORY_DESC",
+		("de_DE", 
+		"LOC_BUILDING_LEU_CONSERVATORY_DESC",					
 		"Verdoppelt [ICON_Gold] Gold und [ICON_Culture] Kulturerträge die angrenzenden Felder durch das Pavillon gewährt werden. Erlaubt der Stadt den Kauf des Naturforschers mit [ICON_FAITH] Glauben.[NEWLINE][NEWLINE]+8 [ICON_Tourism] Tourismus wenn auf atemberaubendem Gelände gebaut."),
-
-		("de_DE",
-		"LOC_PEDIA_BUILDINGS_PAGE_BUILDING_LEU_CONSERVATORY_CHAPTER_HISTORY_PARA_1",
+		
+		("de_DE", 
+		"LOC_PEDIA_BUILDINGS_PAGE_BUILDING_LEU_CONSERVATORY_CHAPTER_HISTORY_PARA_1",					
 		"Ein Gewächshaus ist ein Gebäude oder ein Raum mit Glas oder anderen transparenten Dächern und Wänden, der als Gewächshaus oder Wintergarten genutzt wird. Oft handelt es sich um ein großes, freistehendes Gebäude mit Glaswänden in einem botanischen Garten oder Park, das manchmal auch als Palmenhaus bezeichnet wird, wenn es hoch genug für Bäume ist. Städtische Gewächshäuser wurden im frühen 19. Jahrhundert populär.[NEWLINE][NEWLINE]Viele Städte in kalten Klimazonen, vor allem in Europa, haben städtische Gewächshäuser gebaut, um tropische Pflanzen und Blumenausstellungen zu zeigen. Diese Art von Gewächshaus war zu Beginn des 19. Jahrhunderts sehr beliebt, und gegen Ende des Jahrhunderts wurden sie auch für gesellschaftliche Zwecke genutzt, z. B. für Teegesellschaften. Die Architektur von Wintergärten reicht von typischen viktorianischen Gewächshäusern bis hin zu modernen Stilen wie geodätischen Kuppeln.[NEWLINE][NEWLINE]Wintergärten haben ihren Ursprung im 16. Jahrhundert, als wohlhabende Landbesitzer versuchten, Zitrusfrüchte wie Zitronen und Orangen zu kultivieren, die von Händlern aus wärmeren Regionen des Mittelmeers auf den Tisch gebracht wurden. Die Konservierung von Zitrusfrüchten und anderen zarten Pflanzen begann damit, dass man eine Pergola über Topfpflanzen oder Beete baute oder einfach Topfpflanzen für die kalte Jahreszeit nach drinnen brachte. Weiter nördlich in Europa wurde die Erhaltung von Orangenbäumen zum Trend, und es wurden spezielle Gebäude gebaut, um die schmackhaften, aber empfindlichen Früchte zu schützen. Orangerien, wie sie später genannt wurden, waren in der Regel geschlossene Gebäude aus Holz, Ziegeln oder Stein mit hohen vertikalen Fenstern an den Südwänden. Die Zitrusbäume befanden sich in der Regel in riesigen Töpfen oder Kübeln und wurden für die Sommermonate ins Freie gerollt, wie in den Gärten von Versailles. Die Nutzung dieser Räume weitete sich sowohl gesellschaftlich als auch praktisch aus, da sie zu Unterhaltungszwecken und zur Unterbringung einer größeren Vielfalt von Pflanzen genutzt wurden. Der Begriff Gewächshaus bezeichnete die Räume und Gewächshäuser für zarte Pflanzen.[NEWLINE][NEWLINE]Das 19. Jahrhundert war das goldene Zeitalter des Wintergartenbaus, vor allem in England. Die englischen Gewächshäuser waren das Produkt der englischen Gartenliebe und der neuen Glas- und Heiztechnik. Viele der prächtigen öffentlichen Gewächshäuser, die aus Eisen und Glas gebaut wurden, sind das Ergebnis dieser Ära. Kew Gardens in London ist ein Beispiel für ein großes Gewächshaus, in dem zarte und seltene Pflanzen oder, seltener, Vögel und seltene Tiere gezüchtet werden, wobei Pflanzen und Tiere manchmal zusammen leben."),
 
 --==========================================================================================================================
--- Info
+-- Biósphere and Eiffel Tower
+--==========================================================================================================================		
+		("de_DE",
+		"LOC_BUILDING_BIOSPHERE_EXPANSION2_DESCRIPTION",
+		"+200% [ICON_POWER] Energie für alle Offshore-Windparks, Solarparks, Windparks, Geothermie-Kraftwerke und Hydroelektrische Staudämme. Das Gebäude und die Verbesserungen bieten [ICON_TOURISM] Tourismus in Höhe ihrer [ICON_POWER] Energie. +1 Anziehungskraft für Geländefelder in Eurem Reich, die an Regenwald oder Sumpf angrenzen. Muss angrenzend an einen Fluss und einen Garten mit einem Gewächshaus gebaut werden."),
+
+		("de_DE",
+		"LOC_BUILDING_EIFFEL_TOWER_DESCRIPTION",
+		"Alle Geländefelder in Eurer Zivilisation erhalten +2 Anziehungskraft. Muss auf Flachland angrenzend an einen Garten gebaut werden."),
+
 --==========================================================================================================================
-		("de_DE",
-		"LOC_DISTRICT_LEU_GARDEN_APPEAL_CULTURE_BREATHTAKING",
+-- Info
+--==========================================================================================================================		
+		("de_DE", 
+		"LOC_DISTRICT_LEU_GARDEN_APPEAL_CULTURE_BREATHTAKING",					
 		"+{1_num} [ICON_Culture] Kultur duch Atemberaubende Anziehungskraft. Neue Bezirke können auf Wäldern, Regenwäldern und Schwemmland gebaut werden."),
-
-		("de_DE",
-		"LOC_DISTRICT_LEU_GARDEN_CULTURE",
+		
+		("de_DE", 
+		"LOC_DISTRICT_LEU_GARDEN_CULTURE",					
 		"+{1_num} [ICON_Culture] Kultur durch angrenzende{1_Num : plural 1?n Garten; other? Gärten;}."),
-
-		("de_DE",
-		"LOC_DISTRICT_LEU_GARDEN_SCIENCE",
+		
+		("de_DE", 
+		"LOC_DISTRICT_LEU_GARDEN_SCIENCE",					
 		"+{1_num} [ICON_Science] Wissenschaft durch angrenzende{1_Num : plural 1?n Garten; other? Gärten;}."),
-
-		("de_DE",
-		"LOC_DISTRICT_LEU_GARDEN_FAITH",
+		
+		("de_DE", 
+		"LOC_DISTRICT_LEU_GARDEN_FAITH",					
 		"+{1_num} [ICON_FAITH] Glauben durch angrenzende{1_Num : plural 1?n Garten; other? Gärten;}.");
 
 INSERT OR REPLACE INTO LocalizedText
 		(Language, Tag,	Text)
 VALUES
-		("de_DE",
-		"LOC_NATURALIST_REQUIREMENT",
+		("de_DE", 
+		"LOC_NATURALIST_REQUIREMENT",					
 		"Benötigt zum Kauf ein Gewächshaus."),
-
+		
 		("de_DE",
 		"LOC_PRESERVE_GARDEN_EXCLUSIVE",
 		"Kann nicht in einer Stadt mit Garten gebaut werden."),
-
+		
 		("de_DE",
 		"LOC_GARDEN_PRESERVE_EXCLUSIVE",
 		"Kann nicht in einer Stadt mit Schutzgebiet gebaut werden.");
+
+
+
