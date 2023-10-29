3a67be96de203522022fb9b0ea0cc53e99cf67c5
H.Humpel <H.Humpel@gmx.de>
2023-10-29 11:46:10 +0100

    [FIXED] Bermuda Triangle++

--- CivVIModsGermanTranslations/2197566393/Bermuda_Text_de_DE.sql
+++ CivVIModsGermanTranslations/2197566393/Bermuda_Text_de_DE.sql
@@ -1,12 +1,12 @@
---=============================================================================================================	
+--=============================================================================================================
 -- BERMUDA TRIANGLE++ LOCALISATION
 -- Author: p0kiehl	- ASM Sonntag, 20. August 2023, 23:53:29
 --=============================================================================================================
-INSERT OR REPLACE INTO BaseGameText
-		(Tag,					Text)
-VALUES	('LOC_ABILITY_LOST_AT_SEA_NAME',
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,	Language,				Text)
+VALUES	('LOC_ABILITY_LOST_AT_SEA_NAME', 'de_DE',
 		'Verschollen auf See'),
-		('LOC_ABILITY_LOST_AT_SEA_DESCRIPTION',
+		('LOC_ABILITY_LOST_AT_SEA_DESCRIPTION', 'de_DE',
 		'Verschollen auf See: Darf sofort Ozeanfelder betreten.'),
-		('LOC_FEATURE_BERMUDA_TRIANGLE_DESCRIPTION',
+		('LOC_FEATURE_BERMUDA_TRIANGLE_DESCRIPTION', 'de_DE',
 		'Naturwunder über 3 Felder. Bietet +1 [ICON_Science] Wissenschaft, +1 [ICON_Food] Nahrung und +1 [ICON_Gold] Gold für angrenzende Felder. Einheiten, die es betreten, erhalten die Fähigkeit "Verschollen auf See" (können sofort Ozeanfelder betreten) und werden auf einen weit entfernten Ozean teleportiert. Zusätzlich erhalten Marineeinheiten, die das Naturwunder betreten, die Fähigkeit "Geheimnisvolle Strömungen" (+1 [ICON_Movement] Fortbewegung).');
