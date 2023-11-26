246c04f66428aaa8542867ebcf8191ab3b8468e2
H.Humpel <H.Humpel@gmx.de>
2023-11-26 19:35:42 +0100

    - [FIXED] 6T - Grand Eras

--- CivVIModsGermanTranslations/2769967307/Text/6T_Text_Misc_de_DE.sql
+++ CivVIModsGermanTranslations/2769967307/Text/6T_Text_Misc_de_DE.sql
@@ -11,7 +11,25 @@ VALUES	('de_DE',	'LOC_POLICY_6T_DRESSAGE_NAME',									'Dressur|Dressur|Dressur
 
 INSERT OR REPLACE INTO LocalizedText
 		(Language,	Tag,															Text)
-VALUES	('de_DE',	'LOC_POLICY_AGOGE_DESCRIPTION_6T',								'+50% [ICON_Production] Produktion für Nahkampf-, Kavallerie-Abwehr, Fernkampf- und Aufklärungseinheiten der Antike und Klassik.'),
+VALUES	('de_DE',	'LOC_GREATPERSON_COMBAT_STRENGTH_AOE_CLASSICAL_6T_LAND',		'+5 [ICON_Strength] Kampfstärke und +1 [ICON_Movement] Bewegung für Landeinheiten der Klassik und Spätklassik innerhalb 2 Geländefeldern.'),
+		('de_DE',	'LOC_GREATPERSON_COMBAT_STRENGTH_AOE_CLASSICAL_6T_SEA',			'+5 [ICON_Strength] Kampfstärke und +1 [ICON_Movement] Bewegung für Marineeinheiten der Klassik und Spätklassik innerhalb 2 Geländefeldern.'),
+		('de_DE',	'LOC_GREATPERSON_COMBAT_STRENGTH_AOE_6T_POST_CLASSICAL_LAND',	'+5 [ICON_Strength] Kampfstärke und +1 [ICON_Movement] Bewegung für Landeinheiten der Spätklassik und des Mittelalters innerhalb 2 Geländefeldern.'),
+		('de_DE',	'LOC_GREATPERSON_COMBAT_STRENGTH_AOE_6T_POST_CLASSICAL_SEA',	'+5 [ICON_Strength] Kampfstärke und +1 [ICON_Movement] Bewegung für Marineeinheiten der Spätklassik und des Mittelalters innerhalb 2 Geländefeldern.'),
+			
+		('de_DE',	'LOC_GREAT_PERSON_INDIVIDUAL_JNR_ARCHIMEDES_NAME',				'Archimedes'),
+		('de_DE',	'LOC_JNR_GREATPERSON_EUREKA_STRENGTH',							'[ICON_TechBoosted] Heurekas gewähren 5% mehr der Technologien.'),
+		('de_DE',	'LOC_GREAT_PERSON_INDIVIDUAL_JNR_LI_BING_NAME',					'Li Bing'),
+		('de_DE',	'LOC_JNR_GREATPERSON_DAM_UNLOCK_MATHEMATICS',					'Dämme werden bereits mit der Technologie Mathematik freigeschaltet. Dämme erhalten einen Standard [ICON_PRODUCTION] Produktionsbonus durch Bezirke.'),
+		('de_DE',	'LOC_GREAT_PERSON_INDIVIDUAL_JNR_MA_JUN_NAME',					'Ma Jun'),		
+		('de_DE',	'LOC_JNR_GREATPERSON_GREAT_WORK_WRITING_PRODUCTION',			'+4 [ICON_Production] Produktion surch jede [ICON_GreatWork_WRITING] Große Schrift in dieser Stadt.'),
+		('de_DE',	'LOC_GREAT_PERSON_INDIVIDUAL_JNR_SUSHRUTA_NAME',				'Sushrata'),
+		('de_DE',	'LOC_GREATPERSON_JNR_SUSHRUTA_ACTIVE',							'Löst das [ICON_TechBoosted] Heureka für 1 zufällige Technologie und die [ICON_CivicBoosted] Eingebung für 1 zufällige Ausrichtung der Spätklassik.'),
+		('de_DE',	'LOC_JNR_GREATPERSON_1POSTLCASSICALTECHBOOST',					'Löst das [ICON_TechBoosted] Heureka für {Amount} zufällige {Amount : plural 1?Technologie; other?Technologien;} der Spätklassik.'),
+		('de_DE',	'LOC_JNR_GREATPERSON_1POSTLCASSICALCIVICSBOOST',				'Löst die [ICON_CivicBoosted] Eingebung für {Amount} zufällige {Amount : plural 1?Ausrichtung; other?Ausrichtungen;} der Spätklassik.'),
+		('de_DE',	'LOC_GREATPERSON_EUCLID_ACTIVE_JNR_6T',							'Löst das [ICON_TechBoosted] Heureka für Mathematik und 1 zufällige Technologie der Spätklassik.'),
+		('de_DE',	'LOC_GREATPERSON_3CLASSICALMEDIEVALTECHBOOSTS_6T',				'Löst das [ICON_TechBoosted] Heureka für {Amount} zufällige {Amount : plural 1?Technologie; other?Technologien;} der Spätklassik oder des Mittelalters.'),
+		
+		('de_DE',	'LOC_POLICY_AGOGE_DESCRIPTION_6T',								'+50% [ICON_Production] Produktion für Nahkampf-, Kavallerie-Abwehr, Fernkampf- und Aufklärungseinheiten der Antike und Klassik.'),
 		('de_DE',	'LOC_POLICY_FEUDAL_CONTRACT_DESCRIPTION_6T',					'+50% [ICON_Production] Produktion für Nahkampf-, Kavallerie-Abwehr, Fernkampf- und Aufklärungseinheiten des Mittelalters oder früher.'),
 		('de_DE',	'LOC_POLICY_GRANDE_ARMEE_DESCRIPTION_6T',						'+50% [ICON_Production] Produktion für Nahkampf-, Kavallerie-Abwehr, Fernkampf- und Aufklärungseinheiten des Industriezeitalters oder früher.'),
 		('de_DE',	'LOC_POLICY_MILITARY_FIRST_DESCRIPTION_6T',						'+50% [ICON_Production] Produktion für alle Nahkampf-, Kavallerie-Abwehr, Fernkampf- und Aufklärungseinheiten.'),
